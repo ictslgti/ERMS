3f830cf4b6760d216e468672b849b704ab500711
achchuthany <achch1990@gmail.com>
2020-03-24 19:17:09 +0530

    dashboard

---
+++ dashboard.php
@@ -0,0 +1,35 @@
+<?php
+$title = "Dashboard | Online Examination Result Management System | SLGTI";
+$description = "Online Examination Result  Management System (ERMS)-SLGTI";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include_once("./head.php"); ?>
+</head>
+
+<body>
+    <div class="page-wrapper toggled bg2 border-radius-on light-theme">
+        <nav id="sidebar" class="sidebar-wrapper">
+            <?php include_once("nav.php"); ?>
+        </nav>
+        <main class="page-content pt-2">
+            <div id="overlay" class="overlay"></div>
+            <div class="container-fluid p-5">
+                <!-- #1 Insert Your Content-->
+                <div class="row">
+                    <div class="form-group col-md-12">
+                        <h2>Dashboard</h2>
+                        <p>This is a responsive sidebar template with dropdown menu based on bootstrap framework.</p>
+
+                    </div>
+                </div>
+                <!-- #1 Insert Your Content" -->
+            </div>
+        </main>
+    </div>
+    <?php include_once("script.php"); ?>
+</body>
+
+</html>
