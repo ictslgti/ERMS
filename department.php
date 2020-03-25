8c713546fac452fc8943cb800596a0c315634bbf
Achchuthan Yogarajah <achch1990@gmail.com>
2020-03-25 19:49:54 +0530

    Update and rename add_department.php to department.php

--- department.php
+++ department.php
@@ -1,5 +1,5 @@
 <?php
-$title = "Dashboard | Online Examination Result Management System | SLGTI";
+$title = "Add Department | Online Examination Result Management System | SLGTI";
 $description = "Online Examination Result  Management System (ERMS)-SLGTI";
 ?>
 <!DOCTYPE html>
