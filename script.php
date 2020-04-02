fc620e6a4f46c6b94f87e1bffc4919b60e9c374b
I'm Kavin <kabilraj050@gmail.com>
2020-04-02 13:38:57 -0800

    Update script.php
    online icons from fontawesome #loginform

--- script.php
+++ script.php
@@ -5,4 +5,10 @@
  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
      integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous">
  </script>
+
+
+ <!-- login form online icon script @k-avin -->
+ <script src="https://kit.fontawesome.com/a81368914c.js"></script>
+ <!-- end @k-avin script -->
+ 
  <script src="js/main.js"></script>
