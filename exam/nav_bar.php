3fa6f91df5e083fd8ac6b0ba3872acaddcaf211a
Eashath <seashath@gmail.com>
2020-04-08 17:12:41 +0530

    new form

--- exam/nav_bar.php
+++ exam/nav_bar.php
@@ -41,16 +41,16 @@
   <div class='form-group col-md'>
     <ul class='nav nav-tabs'>
       <li class='nav-item'>
-        <a class='nav-link ' href='exams_dashbord.php'>Home</a>
+        <a class='nav-link ' href='./exams_dashbord.php'>Home</a>
       </li>
       <li class='nav-item'>
-        <a class='nav-link active' href='Exams.php'>Exams</a>
+        <a class='nav-link active' href='./exams.php'>Exams</a>
       </li>
       <li class='nav-item'>
-        <a class='nav-link' href='Exams_results.php'>Update Results</a>
+        <a class='nav-link' href='./exams_results.php'>Update Results</a>
       </li>
       <li class='nav-item'>
-        <a class='nav-link' href='Exams.php'>View Results</a>
+        <a class='nav-link' href='./exams.php'>View Results</a>
       </li>
     </ul>
   </div>
