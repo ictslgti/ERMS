2a6a0e5080e7a73add9ee348c9e6d227d5367c2c
Eashath <seashath@gmail.com>
2020-04-08 16:59:52 +0530

    new form

--- exams_dashbord.php
+++ exams_dashbord.php
@@ -8,42 +8,7 @@ $description = 'Online Examination Result  Management System (ERMS)-SLGTI';
 <head>
     <?php include_once('./head.php');
     ?>
-    <!-- Sample chart -->
-    <!-- <script type = 'text/javascript' src = 'https://www.gstatic.com/charts/loader.js'></script>
-<script type = 'text/javascript'>
-google.charts.load( 'current', {
-    'packages':['corechart']}
-);
-google.charts.setOnLoadCallback( drawChart );
-
-function drawChart() {
-    var data = google.visualization.arrayToDataTable( [
-        ['Year', 'ICT', 'CONT', 'MECHA', 'ELECT', 'FOOT', 'AUTO'],
-        ['2016',  20,  12,    10,    22,    31,   12],
-        ['2017',  12,  34,    14,    33,    5,  12],
-        ['2018',  10,  12,    19,    22,    3,  22],
-        ['2019',  29,  12,    15,    21,    11,   14 ],
-        ['2020',  15,  33,    19,    12,    12,   14 ],
-
-    ] );
-
-    var options = {
-        title: 'STUDENTS PASS RATINGS',
-        curveType: 'function',
-        legend: {
-            position: 'bottom' }
-        }
-        ;
-
-        var chart = new google.visualization.LineChart( document.getElementById( 'curve_chart' ) );
-
-        chart.draw( data, options );
-    }
-    </script>
-    -->
-
 </head>
-
 <body>
     <div class='page-wrapper toggled bg2 border-radius-on light-theme'>
         <nav id='sidebar' class='sidebar-wrapper'>
@@ -51,7 +16,7 @@ function drawChart() {
             ?>
         </nav>
         <main class='page-content pt-2'>
-            <?php include_once('nav_bar.php');
+            <?php include_once('exam/nav_bar.php');
             ?>
             <div id='overlay' class='overlay'></div>
             <div class='container-fluid p-2'>
@@ -123,19 +88,7 @@ function drawChart() {
                         </div>
                     </div>
                 </section>
-
                 <!-- #1 Insert Your Content" -->
-                <!-- <div class="row">
-                    <div class="form-group col-md-12">
-
-                    
-                    <div id="curve_chart' style='width: 1200px;
-    height: 500px"></div>
-
-                    </div>
-                </div> -->
-
-
                 <section class="mt-50">
                     <div class="container-fluid p-2">
                         <div class="row">
@@ -147,7 +100,6 @@ function drawChart() {
                             <div class="col-lg-2 pull-right text-right">
                                 <a href="#' class='primary-btn small fix-gr-bg'> <span class='ti-plus pr-2"></span> Add </a>
                             </div>
-
                             <div class="col-lg-12 shadow-lg p-3 mb-5 bg-white rounded">
                                 <table class="school-table-style w-100">
                                     <thead>
@@ -161,9 +113,7 @@ function drawChart() {
                                     <tbody>
                                         <tr>
                                             <td>
-
                                                 21st octo, 2018
-
                                             </td>
                                             <td>Level -5 Bridging Start</td>
                                             <td>
@@ -172,9 +122,7 @@ function drawChart() {
                                         </tr>
                                         <tr>
                                             <td>
-
                                                 1st Apr, 2020
-
                                             </td>
                                             <td>Following Level 5</td>
                                             <td>
@@ -183,9 +131,7 @@ function drawChart() {
                                         </tr>
                                         <tr>
                                             <td>
-
                                                 1st Apr, 2023
-
                                             </td>
                                             <td>Following Level 5 3rd Year </td>
                                             <td>
@@ -198,8 +144,6 @@ function drawChart() {
                         </div>
                     </div>
                 </section>
-
-
 
             </div>
         </main>
