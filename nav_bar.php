09c4b8dd82e41505f05b8532eed86bbc2ab0241f
Eashath <seashath@gmail.com>
2020-04-08 12:39:48 +0530

    new form

--- nav_bar.php
+++ nav_bar.php
@@ -21,7 +21,7 @@
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
             <a class="dropdown-item" href="#">Admin</a>
             <a class="dropdown-item" href="#">Profile</a>
-            <div class="dropdown-divider"></div>
+            <div class="dropdowndivider"></div>
             <a class="dropdown-item" href="#">Log Out
               <svg class="bi bi-box-arrow-in-left" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M7.854 11.354a.5.5 0 000-.708L5.207 8l2.647-2.646a.5.5 0 10-.708-.708l-3 3a.5.5 0 000 .708l3 3a.5.5 0 00.708 0z" clip-rule="evenodd" />
@@ -36,3 +36,29 @@
     </form>
   </div>
 </nav>
+<br>
+<div class='row'>
+  <div class='form-group col-md'>
+    <ul class='nav nav-tabs'>
+      <li class='nav-item'>
+        <a class='nav-link ' href='exams_dashbord.php'>Home</a>
+      </li>
+      <li class='nav-item'>
+        <a class='nav-link active' href='Exams.php'>Exams</a>
+      </li>
+      <li class='nav-item'>
+        <a class='nav-link' href='Exams_results.php'>Update Results</a>
+      </li>
+      <li class='nav-item'>
+        <a class='nav-link' href='Exams.php'>View Results</a>
+      </li>
+    </ul>
+  </div>
+  <div class='col-md-auto pr-4'>
+    <button type='submit' class='btn btn-success btn-group-sm' name="add" data-toggle='modal' data-target='.bd-example-modal-xl'>
+      <svg class='bi bi-plus-circle-fill' width='2em' height='2em' viewBox='0 0 16 16' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
+        <path fill-rule='evenodd' d='M16 8A8 8 0 110 8a8 8 0 0116 0zM8.5 4a.5.5 0 00-1 0v3.5H4a.5.5 0 000 1h3.5V12a.5.5 0 001 0V8.5H12a.5.5 0 000-1H8.5V4z' clip-rule='evenodd' />
+      </svg>
+      Add Exams</button>
+  </div>
+</div>
