7c7598ea2f7ed744c6e29e61b690e6cd07fb469e
Eashath <seashath@gmail.com>
2020-04-08 17:06:56 +0530

    new form

---
+++ exam/nav_bar.php
@@ -0,0 +1,64 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Exams/update results</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item dropdown dropleft">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <svg class="bi bi-people-circle" width="2em" height="2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 008 15a6.987 6.987 0 005.468-2.63z" />
+              <path fill-rule="evenodd" d="M8 9a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
+              <path fill-rule="evenodd" d="M8 1a7 7 0 100 14A7 7 0 008 1zM0 8a8 8 0 1116 0A8 8 0 010 8z" clip-rule="evenodd" />
+            </svg>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Admin</a>
+            <a class="dropdown-item" href="#">Profile</a>
+            <div class="dropdowndivider"></div>
+            <a class="dropdown-item" href="#">Log Out
+              <svg class="bi bi-box-arrow-in-left" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M7.854 11.354a.5.5 0 000-.708L5.207 8l2.647-2.646a.5.5 0 10-.708-.708l-3 3a.5.5 0 000 .708l3 3a.5.5 0 00.708 0z" clip-rule="evenodd" />
+                <path fill-rule="evenodd" d="M15 8a.5.5 0 00-.5-.5h-9a.5.5 0 000 1h9A.5.5 0 0015 8z" clip-rule="evenodd" />
+                <path fill-rule="evenodd" d="M2.5 14.5A1.5 1.5 0 011 13V3a1.5 1.5 0 011.5-1.5h8A1.5 1.5 0 0112 3v1.5a.5.5 0 01-1 0V3a.5.5 0 00-.5-.5h-8A.5.5 0 002 3v10a.5.5 0 00.5.5h8a.5.5 0 00.5-.5v-1.5a.5.5 0 011 0V13a1.5 1.5 0 01-1.5 1.5h-8z" clip-rule="evenodd" />
+              </svg>
+            </a>
+          </div>
+        </li>
+      </ul>
+
+    </form>
+  </div>
+</nav>
+<br>
+<div class='row'>
+  <div class='form-group col-md'>
+    <ul class='nav nav-tabs'>
+      <li class='nav-item'>
+        <a class='nav-link ' href='exams_dashbord.php'>Home</a>
+      </li>
+      <li class='nav-item'>
+        <a class='nav-link active' href='Exams.php'>Exams</a>
+      </li>
+      <li class='nav-item'>
+        <a class='nav-link' href='Exams_results.php'>Update Results</a>
+      </li>
+      <li class='nav-item'>
+        <a class='nav-link' href='Exams.php'>View Results</a>
+      </li>
+    </ul>
+  </div>
+  <div class='col-md-auto pr-4'>
+    <button type='submit' class='btn btn-success btn-group-sm' name="add" data-toggle='modal' data-target='.bd-example-modal-xl'>
+      <svg class='bi bi-plus-circle-fill' width='2em' height='2em' viewBox='0 0 16 16' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
+        <path fill-rule='evenodd' d='M16 8A8 8 0 110 8a8 8 0 0116 0zM8.5 4a.5.5 0 00-1 0v3.5H4a.5.5 0 000 1h3.5V12a.5.5 0 001 0V8.5H12a.5.5 0 000-1H8.5V4z' clip-rule='evenodd' />
+      </svg>
+      Add Exams</button>
+  </div>
+</div>
