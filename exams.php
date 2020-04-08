42240172120f3fc87691976aacdb4dc02c687706
Eashath <seashath@gmail.com>
2020-04-08 16:55:22 +0530

    new form

--- exams.php
+++ exams.php
@@ -19,7 +19,8 @@ $description = 'Online Examination Result  Management System (ERMS)-SLGTI';
             <?php include_once('nav.php'); ?>
         </nav>
         <main class='page-content pt-2'>
-            <?php include_once('nav_bar.php');
+            <?php include_once('exam/nav_bar.php');
+             include_once('exam/add_exam.php');
             ?>
             <div class='container-fluid p-5'>
                 <!-- #1 Insert Your Content-->
@@ -88,9 +89,9 @@ $description = 'Online Examination Result  Management System (ERMS)-SLGTI';
                 </div>
                 <br>
                 <!--#2 Insert your Content-->
-                <div class='row '>
-                    <div class='col-md'>
-                <table class='table'>
+                <div class="row">
+                    <div class="form-group col-md-12 table-responsive">
+                    <table class='table align-middle'>
                     <thead class='bg-primary text-light'>
                         <tr>
                             <th scope='col'>NO</th>
@@ -140,8 +141,11 @@ $description = 'Online Examination Result  Management System (ERMS)-SLGTI';
         <td> ', $row['course'], '</td>
         <td> ', $row['semester'], '</td>
         <td> ', $row['module'], '<span class = "badge badge-dark">', $row['exam_type'], '</span></td>
-        <td class="btn-group" role="group" > <a class = "btn btn-warning btn-group-sm" href="insert.php?edit=', $row['exam_id'], '"> Edit </a>
-         <a class = "btn btn-danger btn-group-sm" href="?delete=', $row['exam_id'], '"> Delete </a></td>
+        <td class="btn-group" role="group" > 
+        <a class = "btn btn-warning btn-group-sm" href="insert.php?edit=', $row['exam_id'], '"> Edit </a>
+         <a class = "btn btn-danger btn-group-sm" href="?delete=', $row['exam_id'], '"> Delete </a>
+         <a class = "btn btn-info btn-group-sm" href="?view=', $row['exam_id'], '"> View </a>
+         </td>
         </tr>';
                             }
                         } else {
