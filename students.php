f3c7f3d88b45564ac578004245a73fc318eaece6
SathuMathu <ab2237243@gmail.com>
2020-04-10 21:06:21 +0530

    view student details

--- students.php
+++ students.php
@@ -34,9 +34,16 @@ $description = "Online Examination Result Management System (ERMS)-SLGTI";
                 <form method="POST" action="">
                 <div class="row">
                 <div class='col-7'>
-                <?php include_once('student/s_nav_bar.php');
-                include_once('student/add_student.php');
-                ?>
+                <div class='form-group col-md'>
+                <ul class='nav nav-tabs'>
+                <li class='nav-item'>
+                <a class='nav-link active' href='./students.php'>ALL</a>
+                </li>
+                <li class='nav-item'>
+                <a class='nav-link' href='./student.php'>Add New</a>
+                </li>
+                </ul>
+                </div>
                 </div>
                 <div class='col-3'>
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
@@ -49,8 +56,8 @@ $description = "Online Examination Result Management System (ERMS)-SLGTI";
                 
                 <div class="row">
                     <div class="form-group col-md-12 table-responsive">
-                    <table class='table align-middle'>
-                    <thead class='bg-primary text-light'>
+                    <table class='table align-middle '>
+                    <thead class='thead-dark'>
                         <tr>
                             <th scope='col'>NO</th>
                             <th scope='col'>Exams</th>
@@ -59,6 +66,15 @@ $description = "Online Examination Result Management System (ERMS)-SLGTI";
                             <th scope='col'>SEMESTER</th>
                             <th scope='col'>MODULE</th>
                             <th scope='col'>ACTIONS</th>
+                        </tr>
+                        <tr>
+                            <td scope='col'>NO</td>
+                            <td scope='col'>Exams</td>
+                            <td scope='col'>DEPARTMENT</td>
+                            <td scope='col'>COURSE</td>
+                            <td scope='col'>SEMESTER</td>
+                            <td scope='col'>MODULE</td>
+                            <td scope='col'>ACTIONS</td>
                         </tr>
                     </thead>
                     <tbody>
