755c854f35bf9ff0dac556e2a320be003473fffa
Eashath <seashath@gmail.com>
2020-04-11 14:08:17 +0530

    new nav bar changes

--- exams_results.php
+++ exams_results.php
@@ -21,13 +21,14 @@ $description = 'Online Examination Result  Management System (ERMS)-SLGTI';
         </nav>
         <main class='page-content pt-2 '>
             <?php include_once('exam/nav_bar.php');
+            include_once('exam/exresu_nav.php');
             include_once('exam/add_exam.php');
             ?>
             <div id='overlay' class='overlay'></div>
             <div class='container-fluid p-5'>
 
                 <!-- #1 Insert Your Content-->
-               
+
                 <!-- sql -->
                 <!-- #2 Insert Your Content" -->
 
@@ -107,7 +108,7 @@ $description = 'Online Examination Result  Management System (ERMS)-SLGTI';
                             <div class='input-group-sm mb-3'>
                                 <div class='input-group-prepend'>
                                     <!-- <label class = 'input-group-text' for = 'inputGroupSelect01'>betch</label> -->
-                                    <label for='exampleInput'>Academic Year</label>
+                                    <label for='exampleInput'>Betch</label>
                                 </div>
                                 <select class='custom-select' id='inputGroupSelect01' id='validationServer02' required>
                                     <option selected>select betch...</option>
@@ -180,57 +181,58 @@ $description = 'Online Examination Result  Management System (ERMS)-SLGTI';
                 <br>
                 <!-- #3 Insert Your Content" -->
                 <form method='POST' action=''>
-                <div class="row">
-                    <div class="form-group col-md-12 table-responsive">
-                    <table class='table align-middle'>
-                        <thead class='bg-primary text-light'>
-                            <tr>
-                                <th scope='col'>NO</th>
-                                <th scope='col'>REG NO</th>
-                                <th scope='col'>INDEX NO</th>
-                                <th scope='col'>STUDENT NAME</th>
-                                <th scope='col'>ATTEMPT </th>
-                                <th scope='col'>MARKS</th>
-                                <th scope='col'>STATUS</th>
-                                <th scope='col'>VIEW</th>
+                    <div class="row">
+                        <div class="form-group col-md-12 table-responsive">
+                            <table class='table align-middle'>
+                                <thead class='bg-primary text-light'>
+                                    <tr>
+                                        <th scope='col'>NO</th>
+                                        <th scope='col'>REG NO</th>
+                                        <th scope='col'>INDEX NO</th>
+                                        <th scope='col'>STUDENT NAME</th>
+                                        <th scope='col'>ATTEMPT </th>
+                                        <th scope='col'>MARKS</th>
+                                        <th scope='col'>STATUS</th>
+                                        <th scope='col'>VIEW</th>
 
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th scope='row'>1</th>
-                                <td>2018ICTBIT01</td>
-                                <td>2018ICTBIT01</td>
-                                <td>Mohamed Sakkoor Eashath</td>
-                                <td><select class='custom-select' id='inputGroupSelect01' id='validationServer01' required>
-                                        <option value=''>Select</option>
-                                        <option value='1'>1 st</option>
-                                        <option value='2'>2 nd</option>
-                                        <option value='3'>3 rd</option>
-                                </td>
-                                <td><input type='text' class='form-control' placeholder='Marks' id='validationServer02' required aria-label='marks' aria-describedby='addon-wrapping'></td>
-                                <td><span class='badge badge-success'>PASS</span></td>
-                                <td><button type='submit' name='view' class='btn btn-warning btn-sm'>View</button></td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope='row'>1</th>
+                                        <td>2018ICTBIT01</td>
+                                        <td>2018ICTBIT01</td>
+                                        <td>Mohamed Sakkoor Eashath</td>
+                                        <td><select class='custom-select' id='inputGroupSelect01' id='validationServer01' required>
+                                                <option value=''>Select</option>
+                                                <option value='1'>1 st</option>
+                                                <option value='2'>2 nd</option>
+                                                <option value='3'>3 rd</option>
+                                        </td>
+                                        <td>
+                                            <input type='text' class='form-control' placeholder='Marks' id='validationServer02' required aria-label='marks' aria-describedby='addon-wrapping' maxlength="5" size="6">
+                                        </td>
+                                        <td><span class=' badge badge-success'>PASS</span></td>
+                                        <td><button type='button' name='view' class='btn btn-warning btn-sm'>View</button></td>
 
-                            </tr>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <!-- button -->
+                            <div class='row'>
+                                <div class='col-md'>
+                                    <h3 class='text-primary'></h3>
+                                </div>
+                                <div class='col-md'>
 
-                        </tbody>
-                    </table>
-                    <!-- button -->
-                    <div class='row'>
-                        <div class='col-md'>
-                            <h3 class='text-primary'></h3>
-                        </div>
-                        <div class='col-md'>
-
-                        </div>
-                        <div class='col-md-auto'>
-                            <button type='submit' name='cancel' class='btn btn-danger'>CANCEL</button>
-                            <button type='submit' name='save' class='btn btn-primary'>SAVE</button>
+                                </div>
+                                <div class='col-md-auto'>
+                                    <button type='submit' name='cancel' class='btn btn-danger'>CANCEL</button>
+                                    <button type='submit' name='save' class='btn btn-primary'>SAVE</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    </div>
-                </div>
                 </form>
                 <!-- #4 Insert Your Content" -->
                 <!-- #5 Insert Your Content" -->
