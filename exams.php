ceb5e0dc0db0cdf886ff367d1b84f1afccbf6e1c
Eashath <seashath@gmail.com>
2020-04-15 13:39:56 +0530

    new changes

--- exams.php
+++ exams.php
@@ -1,5 +1,5 @@
 <?php
-$title = 'Exams | Online Examination Result Management System | SLGTI';
+$title = 'Exams | ';
 $description = 'Online Examination Result  Management System (ERMS)-SLGTI';
 ?>
 <!DOCTYPE html>
@@ -24,6 +24,7 @@ $description = 'Online Examination Result  Management System (ERMS)-SLGTI';
             include_once('exam/add_exam.php');
             ?>
             <div class='container-fluid p-5'>
+
                 <!-- #1 Insert Your Content-->
                 <!-- <div class="row">
                     <div class="col-md">
@@ -60,9 +61,9 @@ $description = 'Online Examination Result  Management System (ERMS)-SLGTI';
 
                 <!--#2 Insert your Content-->
                 <div class="row">
-                    <div class="form-group col-md-12 table-responsive">
-                        <table class='table align-middle'>
-                            <thead class='bg-primary text-light'>
+                    <div class="form-group col-md-12 table-responsive ">
+                        <table class='table table-md'>
+                            <thead class='table-active text-dark'>
                                 <tr>
                                     <th scope='col'>NO</th>
                                     <th scope='col'>Exams</th>
@@ -108,7 +109,7 @@ $description = 'Online Examination Result  Management System (ERMS)-SLGTI';
                                     <td> ', $row['course'], '</td>
                                     <td> ', $row['semester'], '</td>
                                     <td> ', $row['module'], '<span class = "badge badge-dark">', $row['exam_type'], '</span></td>
-                                    <td class="btn-group" role="group" > 
+                                    <td class="btn-group " role="group" > 
                                     <a class = "btn btn-warning btn-group btn btn-sm " href="insert.php?edit=', $row['exam_id'], '"> Edit </a>
                                     <a class = "btn btn-danger btn-group btn btn-sm " href="?delete=', $row['exam_id'], '"> Delete </a>
                                     <a class = "btn btn-info btn-group btn btn-sm " href="?view=', $row['exam_id'], '"> View </a>
