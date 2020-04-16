e5c6d813f5736a2ae5da1b14fb4ee12a380322fa
JAWSI <irhamuljawsi@gmail.com>
2020-04-16 21:38:51 +0530

    2ndpage

---
+++ attendence_all.php
@@ -0,0 +1,89 @@
+<?php
+$title = "Attendence | Online Examination Result Management System | SLGTI";
+$description = "Online Examination Result  Management System (ERMS)-SLGTI";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include_once("./head.php"); ?>
+</head>
+
+<body>
+    <div class="page-wrapper toggled bg2 border-radius-on light-theme">
+        <nav id="sidebar" class="sidebar-wrapper">
+            <?php include_once("nav.php"); ?>
+        </nav>
+        <main class="page-content pt-2">
+            <div id="overlay" class="overlay"></div>
+            <div class="container-fluid p-5">
+                <!-- #1 Insert Your Content-->
+
+                <div class="container">
+
+                <!-- 1st row start -->
+                <div class="row">
+                     <div class="col-sm">
+                     <div class="border border-primary rounded text-center">
+                     <h2>Student's Attendence | SLGTI</h2>
+                     </div>
+                    </div>
+                    </div>
+                    <br>
+                <!-- 1st row end -->
+
+                <form method="POST" action="">
+                <div class="row">
+                <div class='col-7'>
+                <div class='form-group col-md'>
+                <ul class='nav nav-tabs'>
+                <li class='nav-item'>
+                <a class='nav-link active' href='./attendence_all.php'>ALL Cource</a>
+                </li>
+                <li class='nav-item'>
+                <a class='nav-link' href='./attendence.php'>This course</a>
+                </li>
+                </ul>
+                </div>
+                </div>
+                <div class='col-3'>
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                </div>
+                <div class='col-2'>
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </div>
+                </div>
+                </form>
+                
+                <div class="row">
+                    <div class="form-group col-md-12 table-responsive">
+                    <table class='table align-middle '>
+                    <thead class='thead-dark'>
+                        <tr>
+                            <th scope='col'>Course</th>
+                            <th scope='col'>Taken session</th>
+                            <th scope='col'>Points over taken session</th>
+                            <th scope='col'>Percentage over taken session</th>
+                        </tr>
+                        <tr>
+                            <td scope='col'>BAC</td>
+                            <td scope='col'>21</td>
+                            <td scope='col'>20/21</td>
+                            <td scope='col'>95.2%</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+                    </div>
+                </div>
+
+
+                <!-- #1 Insert Your Content" -->
+            </div>
+        </main>
+    </div>
+    <?php include_once("script.php"); ?>
+</body>
+
+</html>
