9f4c3d67fd3b4be708d38853d70574e326d02bb6
Reggy Kodvin <55224697+kodvinkod@users.noreply.github.com>
2020-04-17 14:27:58 +0530

    Update batch.php

--- batch.php
+++ batch.php
@@ -47,6 +47,7 @@ $description = "Online Examination Result  Management System (ERMS)-SLGTI";
                             <option value="2">Mechanical</option>
                             <option value="2">Food Technology</option>
                             <option value="2">Consruction Technology</option>
+                            <option value="2">Automotieve</option>
                           </select>
                           </div>
                         </div>
