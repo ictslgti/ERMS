a122d6299f4eb90c214fb3410c45c3130fe1011a
sumanan24 <sumanan24@gmail.com>
2020-04-24 14:27:35 +0530

    edit file

--- assessment_result.php
+++ assessment_result.php
@@ -17,153 +17,201 @@ $description = "Online Examination Result  Management System (ERMS)-SLGTI";
         </nav>
         <main class="page-content pt-2">
             <div id="overlay" class="overlay"></div>
+            <?php  
+                  $title ="
+                  <h4 class='text-primary'>ERMS|<span class='text-secondary'>Assessments Result</span></h4>";
+                 include_once('exam/nav_bar.php');
+            
+                 ?>
+
+                
             <div class="container-fluid p-5">
                 <!-- #1 Insert Your Content-->
-                <div class="container">
-<!-- 1 -->
-
                 <div class="row">
-                    <div class="col-md-10 col-sm-10 col-xs-10">
-                     <div class="text-primary">
-                     <h1>Add Assessment</h1>
-                     </div>
+                    <div class="col-md-2 col-sm-4">
+                        <h6>Assessment</h6>
+                    </div>
+                    <div class="col-md-4 col-sm-4">
+                        <h6 class="text-muted">01</h6>
                     </div>
 
-                    <div class="col-md-2 col-sm-2 col-xs-2">
-                    <input type=submit onClick="location.href='assessments.php'" value='view Assessment'class="btn btn-outline-primary">
+                    <div class="col-md-2 col-sm-4">
+                        <h6>Module</h6>
                     </div>
+                    <div class="col-md-4 col-sm-4">
+                        <h6 class="text-muted">MO7<span class="badge badge-dark">Practical</span></h6>
                     </div>
 
-<!-- 1 -->
-                  <hr>
-                  <br>
-                  <br>
-<!-- 2  -->
-
-                    
-                     <form name="myForm" action=""  method="post" required>
-                     <div class="row">
-                     <div class="col-md-6 col-sm-6 col-xs-6">
-                     <div class="form-group">
-                     <div class="input-group mb-3">
-                         <div class="input-group-prepend">
-                         <span class="input-group-text" id="basic-addon1">Department</span>
-                       </div>
-                       <select class="custom-select" id="inputGroupSelect01" name="Department" id="validationCustom04" required>
-                       <option  selected disabled value="">Choose Department</option>
-                            <option value="1">ICT</option>
-                            <option value="2">Mechanical</option>
-                            <option value="3">Food technology</option>
-                         </select>
-                         </div>
-                     </div>
-                     </div>
-                     <div class="col-md-6 col-sm-6 col-xs-6">
-                     <div class="form-group">
-                      <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                          <label class="input-group-text" for="inputGroupSelect01">Course</label>
-                        </div>
-                          <select class="custom-select" id="inputGroupSelect01" name="Course"id="validationCustom04" required>
-                            <option  selected disabled value="">Choose Course</option>
-                            <option value="1">NVQ-LEVEL-5</option>
-                            <option value="2">NVQ-LEVEL-4</option>
-                            <option value="3">BRIDGING</option>
-                          </select>
-                          </div>
-                        </div>
-                      </div>
-                         
-                      
+                    <div class="col-md-2 col-sm-4">
+                        <h6>Batch</h6>
                     </div>
-<br>
-<!-- <2> -->
-<!-- <3> -->
+                    <div class="col-md-4 col-sm-4">
+                        <h6 class="text-muted">Batch-4 <span class="badge badge-dark">2018/2025</span></h6>
+                    </div>
 
-                    <div class="row">
-                        <div class="col-md-6 col-sm-6 col-xs-6">
-                        <div class="form-group">
-                      <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                          <label class="input-group-text" for="inputGroupSelect01">Module</label>
-                        </div>
-                          <select class="custom-select" id="inputGroupSelect01" name="Module"id="validationCustom04" required>
-                            <option  selected disabled value="">Choose Module</option>
-                            <option value="1">M01</option>
-                            <option value="2">EMP-02</option>
-                            <option value="3">EMP-01</option>
-                          </select>
-                          </div>
-                        </div>
+                    <div class="col-md-2 col-sm-4">
+                        <h6>Course </h6>
+                    </div>
+                    <div class="col-md-4 col-sm-4">
+                        <h6 class="text-muted">5it</h6>
+                    </div>
 
+                    <div class="col-md-2 col-sm-4">
+                        <h6>Attempt </h6>
+                    </div>
+                    <div class="col-md-4 col-sm-4">
+                        <h6 class="text-muted">01</h6>
+                    </div>
 
-                        </div>
-                        <div class="col-md-6 col-sm-6 col-xs-6">
-                        <div class="form-group">
-                      <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                          <label class="input-group-text" for="inputGroupSelect01">Assessment</label>
-                        </div>
-                          <select class="custom-select" id="inputGroupSelect01" name="Assessment"id="validationCustom04" required>
-                            <option  selected disabled value="">Choose Assessment</option>
-                            <option value="1">Assessment1</option>
-                            <option value="2">Assessment2</option>
-                            <option value="3">Assessment3</option>
-                          </select>
-                          </div>
-                        </div>
-                        </div>
+                    <div class="col-md-2 col-sm-4">
+                        <h6>Date </h6>
+                    </div>
+                    <div class="col-md-4 col-sm-4">
+                        <h6 class="text-muted"><input type='date'disabled value="2020-02-02"></h6>
+                    </div>
+
+                    <div class="col-md-2 col-sm-4">
+                        <h6>Attempt </h6>
+                    </div>
+                    <div class="col-md-4 col-sm-4">
+                        <h6 class="text-muted">02</h6>
+                    </div>
+
+                    <div class="col-md-2 col-sm-4">
+                        <h6>Date </h6>
+                    </div>
+                    <div class="col-md-4 col-sm-4">
+                        <h6 class="text-muted"><input type='date'disabled value="2020-02-15"></h6>
+                    </div>
+                    <div class="col-md-2 col-sm-4">
+                        <h6>Attempt </h6>
+                    </div>
+                    <div class="col-md-4 col-sm-4">
+                        <h6 class="text-muted">03</h6>
+                    </div>
+
+                    <div class="col-md-2 col-sm-4">
+                        <h6>Date </h6>
+                    </div>
+                    <div class="col-md-4 col-sm-4">
+                        <h6 class="text-muted"><input type='date'></h6>
+                    </div>
+                </div>
+                <br>
+                <form action=""method="POST">
+                <button class="btn btn-outline-primary btn btn-sm "> <a href="?Add"> Add Result </a></button>
+                </form>
+                <br>
+
+<?php
+if(isset($_GET['Add']))
+{
+  ?>  
+    <form method='POST' action='assessment_result.php'>
+    <div class='row'>
+        <div class='form-group col-md-12 table-responsive'>
+            <table class='table align-middle'>
+                <thead class='bg-primary text-light'>
+                    <tr>
+                        <th scope='col'>NO</th>
+                        <th scope='col'>REG NO</th>
+                        <th scope='col'>STUDENT NAME</th>
+                        <th scope='col'>MARKS</th>
+                        <th scope='col'>ATTEMPT </th>
+                        <th scope='col'>STATUS</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope='row'>1</th>
+                        <td>2018ICTBIT30</td> 
+                        <td>S.Sumanan</td>
+                        <td>
+                            <input type='text' class='form-control' placeholder='Marks' id='validationServer02' required aria-label='marks' aria-describedby='addon-wrapping' maxlength='5' size='6'>
+                        </td>
+                        <td><select class='custom-select' id='inputGroupSelect01' id='validationServer01' required>
+                                                <option value=''disabled selected>Select</option>
+                                                <option value='1'>1 st</option>
+                                                <option value='2'>2 nd</option>
+                                                <option value='3'>3 rd</option>
+                                        </td>
+                        <td><span class=' badge badge-success'>PASS</span></td>
                        
-                        </div>
-<!-- 3 -->
 
-<br>
+                    </tr>
+                </tbody>
+            </table>
+            <!-- button -->
+            <div class='row'>
+                <div class='col-md'>
+                    <h3 class='text-primary'></h3>
+                </div>
+                <div class='col-md'>
 
-<!-- <4> -->
+                </div>
+                <div class='col-md-auto'>
+                    <button type='submit' name='cancel' class='btn btn-danger btn btn-sm '>CANCEL</button>
+                    <button type='submit' name='save' class='btn btn-primary btn btn-sm '>SAVE</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
+<?php
+}
+else
+{
+?>
+<form method='POST' action='assessment_result.php'>
+    <div class='row'>
+        <div class='form-group col-md-12 table-responsive'>
+            <table class='table align-middle'>
+                <thead class='bg-primary text-light'>
+                    <tr>
+                        <th scope='col'>NO</th>
+                        <th scope='col'>REG NO</th>
+                        <th scope='col'>STUDENT NAME</th>
+                        <th scope='col'>MARKS</th>
+                        <th scope='col'>STATUS</th>
 
-                   <div class="row">
-                    <div class="col-md-6 col-sm-6 col-xs-6">
-                        <div class="form-group">
-                      <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                          <label class="input-group-text" for="inputGroupSelect01">Batch</label>
-                        </div>
-                          <select class="custom-select" id="inputGroupSelect01" name="Batch"id="validationCustom04" required>
-                            <option  selected disabled value="">Choose Batch</option>
-                            <option value="1">Batch-03</option>
-                            <option value="2">Batch-04</option>
-                            
-                          </select>
-                          </div>
-                        </div>
-                        </div>
-                        
-                       <div class="col-md-6 col-sm-6 col-xs-6">
-                      <div class="form-group">
-                          <div class="input-group mb-3">
-                              <div class="input-group-prepend">
-                                  <span class="input-group-text" id="basic-addon3">Assessment Date</span>
-                              </div>
-                              <input type="date" id="birthday" name="Assessment_Date"class="form-control" id="basic-url" aria-describedby="basic-addon3" name="date"id='validationServer01' required>
-                              </div>
-                        </div>
-                      </div>
-                        </div>
-<!-- 4 -->
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope='row'>1</th>
+                        <td>2018ICTBIT30</td>
+                        <td>S.Sumanan</td>
+                        <td><h6>65 <span class="badge badge-dark">Attempt-1</span></h6></td>
+                        <td><span class=' badge badge-success p-2'>PASS</span></td>
+                    </tr>
 
-              <br>
-             
-                     <div class="row">
-                        <div class="col-md-11 col-sm-11 col-xs-11 "></div>
-                        <div class="col-md-1 col-sm-1 col-xs-1 ">
-                          <input type="submit"  class="btn btn-outline-success "value="Add">
-                         </div>
-                      </div>
-                 </form>
-              </div>
+                    <tr>
+                        <th scope='row'>2</th>
+                        <td>2018ICTBIT05</td>
+                        <td>R.Kogila</td>
+                        <td><h6>80 <span class="badge badge-dark">Attempt-1</span></h6></td>
+                        <td><span class=' badge badge-success p-2'>PASS</span></td>
+                    </tr>
 
-                      
-
+                    <tr>
+                        <th scope='row'>3</th>
+                        <td>2018ICTBIT05</td>
+                        <td>R.Kodvin</td>
+                        <td> <h6>75 <span class="badge badge-dark">Attempt-2</span></h6></td>
+                        <td><span class=' badge badge-success p-2'>PASS</span></td>
+                    </tr>
+                </tbody>
+            </table>
+            <!-- button -->
+            
+        </div>
+    </div>
+</form>
+    
+<?php
+}
+?>
 
                 <!-- #1 Insert Your Content" -->
 
