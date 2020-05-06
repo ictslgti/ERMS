b3428ed0d0dc5087dd31f17960b64060ff07dee2
sumanan24 <48612693+sumanan24@users.noreply.github.com>
2020-05-06 11:25:06 +0530

    add
    add

---
+++ lecturers/exam/exams_nav.php
@@ -0,0 +1,36 @@
+<div class='row'>
+    <div class='form-group col-md'>
+        <ul class='nav nav-tabs'>
+            <li class='nav-item'>
+                <a class='nav-link ' href='./exams_dashbord.php'>Home</a>
+            </li>
+            <li class='nav-item'>
+                <a class='nav-link  active' href='./exams.php'>Exams</a>
+            </li>
+            <li class='nav-item'>
+                <a class='nav-link ' href='./exams_results.php'>Update Results</a>
+            </li>
+            <li class='nav-item'>
+                <!-- <a class='nav-link active ' href='./exam_report.php'>View Results</a> -->
+                <div class="dropdown show">
+
+                    <a class="nav-link  dropdown-toggle" href="./exam_report.php" role="button" aria-haspopup="true" aria-expanded="false">
+                        View Results
+                    </a>
+
+                    <!-- <div class="dropdown-menu nav-item" aria-labelledby="dropdownMenuLink">
+                        <a class="dropdown-item" id="academic-tab" data-toggle="tab" href="#academic" role="tab" aria-controls="home" aria-selected="true">Academic Transcript</a>
+                        <a class="dropdown-item nav-link" id="tvec-tab" data-toggle="tab" href="#tvec" role="tab" aria-controls="tvec" aria-selected="false">TVEC Exams Results</a>
+                        <a class="dropdown-item nav-link" id="institute-tab" data-toggle="tab" href="#institute" role="tab" aria-controls="institute" aria-selected="false">INSTITUTE Exams Results</a>
+                        <a class="dropdown-item nav-link" id="student-tab" data-toggle="tab" href="#student" role="tab" aria-controls="student" aria-selected="false">Students Reports</a>
+                    </div> -->
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class='col-md-auto pr-4'>
+        <button type='submit' class='btn btn-success btn-group btn btn-sm ' name="add" data-toggle='modal' data-target='#add_exam'>
+            <i class="fas fa-plus" style="font-size: 18px">
+            </i>
+            Exams</button> </div>
+</div>
---
+++ lecturers/nav.php
@@ -0,0 +1,389 @@
+<div class="sidebar-content">
+    <!-- sidebar-brand  -->
+    <div class="sidebar-item sidebar-brand">
+        <a href="#">ERMS @ SLGTI</a>
+        <div id="close-sidebar">
+            <i class="fas fa-times"></i>
+        </div>
+    </div>
+
+    <!-- sidebar-header  -->
+    <div class="sidebar-item sidebar-header d-flex flex-nowrap">
+        <div class="user-pic">
+            <img class="img-responsive img-rounded" src="img/user.jpg" alt="User picture">
+        </div>
+        <div class="user-info">
+            <span class="user-name">ICT
+                <strong>Admin</strong>
+            </span>
+            <span class="user-role">Achchuthan</span>
+            <span class="user-status">
+
+                <span>Online</span>
+            </span>
+        </div>
+    </div>
+    <!-- sidebar-search  -->
+    <div class="sidebar-item sidebar-search">
+        <div>
+            <div class="input-group">
+                <input type="text" class="form-control search-menu" placeholder="Search...">
+                <div class="input-group-append">
+                    <span class="input-group-text">
+                        <i class="fa fa-search" aria-hidden="true"></i>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- sidebar-menu  -->
+    <div class=" sidebar-item sidebar-menu">
+        <ul>
+            <li class="header-menu">
+                <span>General</span>
+            </li>
+            <li class="sidebar-dropdown">
+                <a href="#">
+                    <i class="fa fa-tachometer-alt"></i>
+                    <span class="menu-text">Dashboard</span>
+                    <!-- <span class="badge badge-pill badge-warning">New</span> -->
+                </a>
+                <div class="sidebar-submenu">
+                    <ul>
+                        <li>
+                            <a href="#">Admin
+                                <span class="badge badge-pill badge-success">Pro</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">Lecturers</a>
+                        </li>
+                        <!-- <li>
+                            <a href="#">Students</a>
+                        </li> -->
+                    </ul>
+                </div>
+            </li>
+            <li class="sidebar-dropdown">
+                <a href="#">
+                    <i class="fa fa-university"></i>
+                    <span class="menu-text">Department</span>
+                    <span class="badge badge-pill badge-danger">6</span>
+                </a>
+                <div class="sidebar-submenu">
+                    <ul>
+                        <li>
+                            <a href="department.php">Departments
+
+                            </a>
+                        </li>
+                        <li>
+                            <a href="department.php"><i class="fas fa-plus"></i>New Department</a>
+
+                        </li>
+                        <li>
+                            <a href="Academicyear.php">Betch Deatils</a>
+
+                        </li>
+                        <li>
+                            <a href="Academicyears.php"><i class="fas fa-plus"></i>New Betch</a>
+                        </li>
+                        <li>
+                            <a href="Academicyear.php">Academic year Deatils</a>
+
+                        </li>
+                        <li>
+                            <a href="Academicyears.php"><i class="fas fa-plus"></i>New Academic year</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="sidebar-dropdown">
+                <a href="#">
+                    <i class="fa fa-book-open"></i>
+                    <span class="menu-text">Course</span>
+
+                </a>
+                <div class="sidebar-submenu">
+                    <ul>
+                        <li>
+                            <a href="course.php">Course Deatils
+
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fas fa-plus"></i>New Course</a>
+
+                        </li>
+                        <li>
+                            <a href="module.php">Module Deatils</a>
+
+                        </li>
+                        <li>
+                            <a href="#"><i class="fas fa-plus"></i>New Module</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </li>
+            <li class="sidebar-dropdown">
+                <a href="#">
+                    <i class="fas fa-user-tie"></i>
+                    <span class="menu-text">Stafs</span>
+                </a>
+                <div class="sidebar-submenu">
+                    <ul>
+                        <li>
+                            <a href="#">stafs Details</a>
+                        </li>
+                        <li>
+                            <a href="#">staf Entrolment</a>
+                        </li>
+                        <li>
+                            <a href="#">Staf Exit</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="sidebar-dropdown">
+                <a href="#">
+                    <i class="fas fa-user-graduate"></i>
+                    <span class="menu-text">Students</span>
+                </a>
+                <div class="sidebar-submenu">
+                    <ul>
+                        <li>
+                            <a href="student.php">student Details</a>
+                        </li>
+                        <li>
+                            <a href="students.php"><i class="fas fa-plus"></i>Students Entrol</a>
+                        </li>
+                        <li>
+                            <a href="#">student Report</a>
+                        </li>
+                        <li>
+                            <a href="#">--</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="sidebar-dropdown">
+                <a href="#">
+                    <i class="fas fa-pen-square"></i>
+                    <span class="menu-text">Exams</span>
+                </a>
+                <div class="sidebar-submenu">
+                    <ul>
+                        <li>
+                            <a href="exams_dashbord.php">Exams</a>
+                        </li>
+                        <li>
+                            <a href="exam_report.php">Exams Report</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="sidebar-dropdown">
+                <a href="#">
+                    <i class="fa fa-award"></i>
+                    <span class="menu-text">Assesment</span>
+                </a>
+                <div class="sidebar-submenu">
+                    <ul>
+                        <li>
+                            <a href="assessment.php">Assesment Details</a>
+                        </li>
+                        <li>
+                            <a href="assessments.php">Assesments</a>
+                        </li>
+                        <li>
+                            <a href="assessment_type.php">Assesment Type</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="sidebar-dropdown">
+                <a href="#">
+                    <i class="fa fa-chalkboard-teacher"></i>
+                    <span class="menu-text">Attendence</span>
+                </a>
+                <div class="sidebar-submenu">
+                    <ul>
+                        <li>
+                            <a href="attendence.php">Attendence Details</a>
+                        </li>
+                        <li>
+                            <a href="#">Attendence Update</a>
+                        </li>
+                        <li>
+                            <a href="#">Attendence Report</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="header-menu">
+                <span>Extra</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-book"></i>
+                    <span class="menu-text">Documentation</span>
+                    <span class="badge badge-pill badge-primary">Beta</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-calendar"></i>
+                    <span class="menu-text">Calendar</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-folder"></i>
+                    <span class="menu-text">Examples</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <!-- sidebar-menu  -->
+</div>
+<!-- sidebar-footer  -->
+<!-- <div class="sidebar-footer">
+    <div class="dropdown">
+
+        <a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fa fa-bell"></i>
+            <span class="badge badge-pill badge-warning notification">3</span>
+        </a>
+        <div class="dropdown-menu notifications" aria-labelledby="dropdownMenuMessage">
+            <div class="notifications-header">
+                <i class="fa fa-bell"></i> Notifications
+            </div>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">
+                <div class="notification-content">
+                    <div class="icon">
+                        <i class="fas fa-check text-success border border-success"></i>
+                    </div>
+                    <div class="content">
+                        <div class="notification-detail">Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. In totam explicabo</div>
+                        <div class="notification-time">
+                            6 minutes ago
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <a class="dropdown-item" href="#">
+                <div class="notification-content">
+                    <div class="icon">
+                        <i class="fas fa-exclamation text-info border border-info"></i>
+                    </div>
+                    <div class="content">
+                        <div class="notification-detail">Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. In totam explicabo</div>
+                        <div class="notification-time">
+                            Today
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <a class="dropdown-item" href="#">
+                <div class="notification-content">
+                    <div class="icon">
+                        <i class="fas fa-exclamation-triangle text-warning border border-warning"></i>
+                    </div>
+                    <div class="content">
+                        <div class="notification-detail">Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. In totam explicabo</div>
+                        <div class="notification-time">
+                            Yesterday
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item text-center" href="#">View all notifications</a>
+        </div>
+    </div>
+    <div class="dropdown">
+        <a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fa fa-envelope"></i>
+            <span class="badge badge-pill badge-success notification">7</span>
+        </a>
+        <div class="dropdown-menu messages" aria-labelledby="dropdownMenuMessage">
+            <div class="messages-header">
+                <i class="fa fa-envelope"></i> Messages
+            </div>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">
+                <div class="message-content">
+                    <div class="pic">
+                        <img src="img/user.jpg" alt="">
+                    </div>
+                    <div class="content">
+                        <div class="message-title">
+                            <strong> Jhon doe</strong>
+                        </div>
+                        <div class="message-detail">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            In totam explicabo</div>
+                    </div>
+                </div>
+
+            </a>
+            <a class="dropdown-item" href="#">
+                <div class="message-content">
+                    <div class="pic">
+                        <img src="img/user.jpg" alt="">
+                    </div>
+                    <div class="content">
+                        <div class="message-title">
+                            <strong> Jhon doe</strong>
+                        </div>
+                        <div class="message-detail">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            In totam explicabo</div>
+                    </div>
+                </div>
+
+            </a>
+            <a class="dropdown-item" href="#">
+                <div class="message-content">
+                    <div class="pic">
+                        <img src="img/user.jpg" alt="">
+                    </div>
+                    <div class="content">
+                        <div class="message-title">
+                            <strong> Jhon doe</strong>
+                        </div>
+                        <div class="message-detail">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            In totam explicabo</div>
+                    </div>
+                </div>
+            </a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item text-center" href="#">View all messages</a>
+
+        </div>
+    </div>
+    <div class="dropdown">
+        <a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fa fa-cog"></i>
+            <span class="badge-sonar"></span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuMessage">
+            <a class="dropdown-item" href="#">My profile</a>
+            <a class="dropdown-item" href="#">Help</a>
+            <a class="dropdown-item" href="#">Setting</a>
+        </div>
+    </div>
+    <div>
+        <a href="#">
+            <i class="fa fa-power-off"></i>
+        </a>
+    </div>
+    <div class="pinned-footer">
+        <a href="#">
+            <i class="fas fa-ellipsis-h"></i>
+        </a>
+    </div>
+</div> -->
