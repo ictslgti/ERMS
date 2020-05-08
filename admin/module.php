557b6e9cc8b7466e5a432ea7a38b3d6443c8328f
K-O-G-I-L-A <55224732+K-O-G-I-L-A@users.noreply.github.com>
2020-05-08 12:14:02 +0530

    Update module.php

--- admin/module.php
+++ admin/module.php
@@ -25,7 +25,7 @@ $description = "Online Examination Result  Management System (ERMS)-SLGTI";
                 <div class="row">
                 <div class="col"><h4>Module</h4></div>
                 <div class="col-auto">
-                <button type="submit" class="btn btn-outline-primary">Modules</button>
+                <a href="modules.php" class="btn btn-outline-primary">Modules</a>
                 </div>
 
                 </div>
