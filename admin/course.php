febdb705fe031f79bedde2f56fcba3102288d956
K-O-G-I-L-A <55224732+K-O-G-I-L-A@users.noreply.github.com>
2020-05-08 11:00:58 +0530

    edit
    edit

--- admin/course.php
+++ admin/course.php
@@ -16,129 +16,133 @@ $description = "Online Examination Result  Management System (ERMS)-SLGTI";
        
         <!-- <main class="page-content pt-2"> -->
             <!-- 1st row start -->
-            <div class="container">
-            <form method="POST" action="">
-            <div class="card  mb-3">
-  <div class="card-header">
-  <div class="row">
-  <div class="col"><h4>Course</h4></div>
-  <div class="col-auto"><button type="submit" class="btn btn-outline-primary">
- Courses 
- </button>
- </div>
- </div>
-  <div class="card-body ">
-    
 
-  <div class="card  mb-3">
-  <div class="card-body ">
-    
-                     
-                     <!-- 2 row end -->
-                     <div class="card  mb-3">
-                     
-                     <div class="card-body ">
+         <div class="container"> 
+         <form action=""> 
+         <br>
+            <div class="card  mb-3" >
+                <div class="card-header ">
+                <div class="row">
+                <div class="col"><h4>Course</h4></div>
+                <div class="col-auto">
+                <button type="submit" class="btn btn-outline-primary">Courses</button>
+                </div>
+
+                </div>
+                </div>
+                <div class="card-body ">
+                <div class="card  mb-3">
+                   
+                    <div class="card-body ">
+                    <form method="POST" action="">
                      <div class="row">
                      <div class="col-sm">
+                     Code  <br>
                      <div class="form-group">
-                     <div class="input-group mb-3">
-                         <div class="input-group-prepend">
-                         <span class="input-group-text" id="basic-addon1">Code</span>
-                       </div>
-                       <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" id="validationServer01" required>
-                         </div>
+                     <div class="input-group input-group-sm mb-3">
+                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"id="validationServer01" required>
+                        </div>
                          </div>
                      </div>
                     
 
                      <div class="col-sm">
                      <div class="form-group">
-                     <div class="input-group mb-3">
-                         <div class="input-group-prepend">
-                         <span class="input-group-text" id="basic-addon1">Name</span>
-                       </div>
-                       <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" id="validationServer01" required>
+                     Name  <br>
+                     <div class="input-group input-group-sm mb-3">
+                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"id="validationServer01" required>
+                        </div>
                          </div>
-                     </div>
-                     </div>
                      </div>
                      <br>
-                     <div class="row">
+                    </div>
+<!-- 2 row end -->
+
+
+                    <div class="row">
                      <div class="col-sm">
+                     Duration Of Course  <br>
                      <div class="form-group">
-                     <div class="input-group mb-3">
-                         <div class="input-group-prepend">
-                         <span class="input-group-text" id="basic-addon1">Duration Of Course</span>
-                       </div>
-                       <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" id="validationServer01" required>
-                         </div>
+                     <div class="input-group input-group-sm mb-3">
+                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"id="validationServer01" required>
+                        </div>
                          </div>
                      </div>
                     
 
                      <div class="col-sm">
                      <div class="form-group">
-                     <div class="input-group mb-3">
-                         <div class="input-group-prepend">
-                         <span class="input-group-text" id="basic-addon1">Duration Of IJT</span>
-                       </div>
-                       <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" id="validationServer01" required>
+                     Duration Of OJT  <br>
+                     <div class="input-group input-group-sm mb-3">
+                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"id="validationServer01" required>
+                        </div>
                          </div>
-                     </div>
-                     </div>
                      </div>
                      <br>
                      
-                     <div class="row">
+                    </div>
+                   <!-- 3 row end -->
+
+
+                   <div class="row">
                      <div class="col-sm">
+                     NVQ Level  <br>
                      <div class="form-group">
-                     <div class="input-group mb-3">
-                         <div class="input-group-prepend">
-                         <span class="input-group-text" id="basic-addon1">NVQ Level</span>
-                       </div>
-                       <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" id="validationServer01" required>
-                         </div>
+                     <div class="input-group input-group-sm mb-3">
+  
+                        <select class="custom-select" id="inputGroupSelect01"id="validationCustom04" required>
+                        <option selected disabled value="">Choose NVQ Level </option>
+                            <option value="1">NVQ-04</option>
+                            <option value="1">NVQ-05 Bridging</option>
+                            <option value="1">NVQ-05</option>
+                            <option value="1">NVQ-06</option>
+                            <option value="1">NVQ-07</option>
+                            
+                        </select>
+                        </div>
                          </div>
                      </div>
                     
 
                      <div class="col-sm">
                      <div class="form-group">
-                     <div class="input-group mb-3">
-                         <div class="input-group-prepend">
-                         <span class="input-group-text" id="basic-addon1">Department</span>
-                       </div>
-                       <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" id="validationServer01" required>
+                     Department  <br>
+                     <div class="input-group input-group-sm mb-3">
+  
+                        <select class="custom-select" id="inputGroupSelect01"id="validationCustom04" required>
+                        <option selected disabled value="">Choose  Department  </option>
+                            <option value="1">ICT</option>
+                            <option value="2">AUT</option>
+                            <option value="3">CON</option>
+                        </select>
+                        </div>
                          </div>
                      </div>
-                     </div>
-                     </div>
+                     <br>
                      
+                    </div>
 
-                     <div class="row">
-  </div>
-</div>
-  </div>
-  <div class="card-footer bg-transparent ">  <div class="col-11 "></div>
+
+
+
+                    </div>
+
+                    
+                </div>
+                <div class="card-footer "><div class="row">
+                     <div class="col-11 "></div>
                      <div class="col-1">
                      <button type="submit" class="btn btn-outline-success" data-toggle="modal"
                                 data-target="#exampleModal">
                                 Add
                             </button>
-                     </div>
-                     </div> </div>
-</div>
-                <!-- 1st row end -->
-            <div id="overlay" class="overlay"></div>
-            <div class="container-fluid p-5">
-                <!-- #1 Insert Your Content-->
-               
+                     </div></div>
+                </div>
                 
-                  
-                    <!-- 2 row start -->
                     
                      
-                   
+                     
+                     </div>
 </form>
                     
 
