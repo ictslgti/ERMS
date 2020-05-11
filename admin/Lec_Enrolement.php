24f88747fd9bfe9cec5c1fd092368d4259b3499b
K-O-G-I-L-A <55224732+K-O-G-I-L-A@users.noreply.github.com>
2020-05-11 20:16:15 +0530

    Update Lec_Enrolement.php

--- admin/Lec_Enrolement.php
+++ admin/Lec_Enrolement.php
@@ -25,7 +25,7 @@ $description = "Online Examination Result  Management System (ERMS)-SLGTI";
                 <div class="row">
                 <div class="col"><h4>Module Enrollment</h4></div>
                 <div class="col-auto">
-                <!-- <a href="modules.php" class="btn btn-outline-primary">Modules</a> -->
+                <a href="#" class="btn btn-outline-primary">Modules</a>
                 </div>
 
                 </div>
@@ -46,21 +46,6 @@ $description = "Online Examination Result  Management System (ERMS)-SLGTI";
                      </div>
                     
 
-                     <!-- <div class="col-sm">
-                     <div class="form-group">
-                      Lecturer Name  <br>
-                     <div class="form-group">
-                     <div class="input-group input-group-sm mb-3">
-                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"id="validationServer01" required>
-                        </div>
-                         </div>
-                     </div>
-                    </div>
-                    </div> -->
-                    <!-- 1 row end -->
-                    
-                    
-
                      <div class="col-sm">
                      <div class="form-group">
                      Course Name  <br>
@@ -77,11 +62,10 @@ $description = "Online Examination Result  Management System (ERMS)-SLGTI";
                          </div>
                      </div>
                      
-                     
                     </div>
-
-                    
                 </div>
+
+
                 <div class="row">
                 <div class="col-sm">
                      <div class="form-group">
@@ -98,13 +82,9 @@ $description = "Online Examination Result  Management System (ERMS)-SLGTI";
                         </div>
                          </div>
                      </div>
-                     
-                     
-                    <!-- </div> -->
-
-                    
+                        
                 </div>
-                <!-- <div class="row"> -->
+               
                      <div class="col-sm">
                      Academic Year  <br>
                      <div class="form-group">
@@ -121,108 +101,10 @@ $description = "Online Examination Result  Management System (ERMS)-SLGTI";
                         </div>
                          </div>
                      </div>
-
-                <!-- 2 row end -->
-
-                <!-- <div class="row">
-                     <div class="col-sm">
-                     Learning Hours <br>
-                     <div class="form-group">
-                     <div class="input-group input-group-sm mb-3">
-                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"id="validationServer01" required>
-                        </div>
-                         </div>
                      </div>
-                    
-
-                     <div class="col-sm">
-                     <div class="form-group">
-                     Lecture Hours  <br>
-                     <div class="form-group">
-                     <div class="input-group input-group-sm mb-3">
-                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"id="validationServer01" required>
-                        </div>
-                         </div>
                      </div>
-                     
-                     
-                    </div>
-
-                    
-                </div>
-
-                <!-- 3 row end -->
-
-
-                <!-- <div class="row">
-                     <div class="col-sm">
-                     Practical Hours <br>
-                     <div class="form-group">
-                     <div class="input-group input-group-sm mb-3">
-                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"id="validationServer01" required>
-                        </div>
-                         </div>
-                     </div>
-                    
-
-                     <div class="col-sm">
-                     <div class="form-group">
-                     Self Study Hours <br>
-                     <div class="form-group">
-                     <div class="input-group input-group-sm mb-3">
-                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"id="validationServer01" required>
-                        </div>
-                         </div>
-                     </div>
-                     
-                     
-                    </div>
-
-                    
-                </div> -->
-
-                <!-- 4 row end -->
-<!-- 6 row start -->
-<!-- <div class="row">
-                     <div class="col-sm">
-                    Module_aim
-                     <div class="form-group">
-                     <div class="input-group mb-3">
-                         <textarea name="AIM" id="" cols="30" rows="10"></textarea>
-                         </div>
-                         </div>
-                     </div>
-                    
-                     <div class="col-sm">
-                    Learning _Outcomes
-                     <div class="form-group">
-                     <div class="input-group mb-3">
-                         <textarea name="Learn" id="" cols="30" rows="10"></textarea>
-                         </div>
-                         </div>
-                     </div>
-                     <div class="col-sm">
-                    Resources
-                     <div class="form-group">
-                     <div class="input-group mb-3">
-                         <textarea name="Resources" id="" cols="30" rows="10"></textarea>
-                         </div>
-                         </div>
-                     </div>
-                     <div class="col-sm">
-                    References
-                     <div class="form-group">
-                     <div class="input-group mb-3">
-                         <textarea name="References" id="" cols="30" rows="10"></textarea>
-                         </div>
-                         </div>
-                     </div>
-                 
-                     </div> --> 
-
-
-<!-- end -->    
-<div class="card-footer "><div class="row">
+                
+                      <div class="card-footer "><div class="row">
                      <div class="col-11 "></div>
                      <div class="col-1">
                      <button type="submit" class="btn btn-outline-success" data-toggle="modal"
@@ -231,12 +113,7 @@ $description = "Online Examination Result  Management System (ERMS)-SLGTI";
                             </button>
                      </div></div>
                 </div>
-                
-                    
-                     
-                     
-                    
-</form>
+                </form>
                     
 
 
