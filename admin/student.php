84d9aa731a9cb0089239fef2306bb5aa3c82fca0
SathuMathu <ab2237243@gmail.com>
2020-05-12 22:50:15 +0530

    add student form

--- admin/student.php
+++ admin/student.php
@@ -14,11 +14,125 @@ $description = "Online Examination Result Management System (ERMS)-SLGTI";
 <body>
     <div class="page-wrapper toggled bg2 border-radius-on light-theme">
             <?php include_once("nav.php"); ?>
-            
-        <main class="">
-            <div id="overlay" class="overlay"></div>
-            <div class="container-fluid p-5">
+            <!-- insert  start-->
+            <?php
+            if(
+                isset($_POST['add'])  
+                && !empty($_POST['regno'])
+                && !empty($_POST['title'])
+                && !empty($_POST['fullname'])
+                && !empty($_POST['ini_name']) 
+                && !empty($_POST['gender']) 
+                && !empty($_POST['civil']) 
+                && !empty($_POST['email']) 
+                && !empty($_POST['nic']) 
+                && !empty($_POST['dob']) 
+                && !empty($_POST['phone']) 
+                && !empty($_POST['address']) 
+                && !empty($_POST['ds'])
+                && !empty($_POST['district']) 
+                && !empty($_POST['province']) 
+                && !empty($_POST['zip'])
+                && !empty($_POST['blood']) 
+                && !empty($_POST['gname'])
+                && !empty($_POST['gaddress']) 
+                && !empty($_POST['gphone']) 
+                && !empty($_POST['grelation'])
+            )
+            {
+                $regno = $_POST['regno'];
+                $title = $_POST['title'];
+                $fullname = $_POST['fullname'];
+                $ini_name = $_POST['ini_name'];
+                $gender = $_POST['gender'];
+                $civil = $_POST['civil'];
+                $email = $_POST['email'];
+                $nic = $_POST['nic'];
+                $dob = $_POST['dob'];
+                $phone = $_POST['phone'];
+                $address = $_POST['address'];
+                $ds = $_POST['ds'];
+                $district = $_POST['district'];
+                $province = $_POST['province'];
+                $zip = $_POST['zip'];
+                $blood = $_POST['blood'];
+                $gname = $_POST['gname'];
+                $gaddress = $_POST['gaddress'];
+                $gphone = $_POST['gphone'];
+                $grelation = $_POST['grelation'];
 
+                $sqlstudent = "INSERT INTO student (id, title, full_name, name_with_initials, gender, civil_status, email, nic, date_of_birth, 
+                phone_no, address, divisional_secretariat, district, province, zip_code, blood_group, guardian_name, guardian_address, guardian_phone_no,
+                guardian_relationship) VALUES 
+                ('$regno','$title','$fullname','$ini_name','$gender','$civil','$email','$nic','$dob','$phone',
+                '$address','$ds','$district','$province','$zip','$blood','$gname','$gaddress','$gphone','$grelation')";
+
+            if (mysqli_query($con,$sqlstudent)) 
+            {
+                echo '<div class="alert alert-success" role="alert">
+                New record created successfully
+                Insert Successfully
+                </div>';
+                } else 
+                {
+                    echo "Error: " . $sqlstudent . "<br>" . mysqli_error($con);
+                }
+            }
+
+            if(
+                isset($_POST['add'])
+                && !empty($_POST['regno']) 
+                && !empty($_POST['cid'])
+                && !empty($_POST['ayear']) 
+                && !empty($_POST['mode']) 
+                && !empty($_POST['status']) 
+                && !empty($_POST['enrolldate']) 
+                && !empty($_POST['exitdate']))
+                {
+                    $regno = $_POST['regno'];
+                    $cid=$_POST['cid'];
+                    $ayear=$_POST['ayear'];
+                    $mode=$_POST['mode'];
+                    $status=$_POST['status'];
+                    $enrolldate=$_POST['enrolldate'];
+                    $exitdate=$_POST['exitdate'];
+
+                    $sqlenroll = "INSERT INTO student_enroll (id, course_id, academic_year, course_mode, student_status,
+                    enroll_date, exit_date) VALUES ('$regno','$cid','$ayear','$mode','$status','$enrolldate','$exitdate')";
+
+                        if(mysqli_query($con,$sqlenroll))
+                        {
+                        // echo '<div class="alert alert-success" role="alert">
+                        // Record Insert Successfully
+                        // </div>';
+                        }
+                        else
+                        {
+                        echo "Error: " . $sqlenroll . "<br>" . mysqli_error($con);
+                        }
+                }
+            ?>
+            <!-- insert  end -->
+
+            <!-- edit  start -->
+            <?php
+            $full_name = null;
+            if (isset($_GET['edit'])) {
+                $student_id = $_GET['edit'];
+                $sql_student = "SELECT * FROM student LEFT JOIN student_enroll
+                ON `student`.`id` = `student_enroll`.`id`
+                WHERE `student`.`id` = '$student_id'";
+                $result_student = mysqli_query($con, $sql_student);
+                $row_fullname = mysqli_fetch_assoc($result_student);
+                if (mysqli_num_rows($result_student) == 1 ) {
+                    $full_name = $row_fullname['full_name'];
+                }   
+            }
+                ?>
+            <!-- insert  end -->
+
+        <main class="">
+            <div class="container-fluid p-5">
                 <!-- #1 Insert Your Content-->
                 <div class="container">
 
@@ -72,7 +186,7 @@ $description = "Online Examination Result Management System (ERMS)-SLGTI";
 
                     <div class="col-7">
                     <label for="fullname"> Full Name: </label>
-                    <input type="text" class="form-control" name="fullname" placeholder="Sathyaseelan Sathursan" required>
+                    <input type="text" class="form-control" name="fullname" placeholder="Sathyaseelan Sathursan" value="<?php echo $student_name; ?>" required>
                     </div>
 
                     <div class="col-3">
@@ -198,7 +312,7 @@ $description = "Online Examination Result Management System (ERMS)-SLGTI";
 
                     <div class="col-2">
                     <label for="zip"> ZIP-Code:</label>
-                    <input type="text" class="form-control" name="zip" placeholder="32420"  required>
+                    <input type="text" class="form-control" name="zip" placeholder="32420">
                     </div>
 
                     <div class="col-2">
@@ -328,17 +442,17 @@ $description = "Online Examination Result Management System (ERMS)-SLGTI";
 
                     <div class="col-3">
                     <label for="gname">Name :</label>
-                    <input type="text" class="form-control" name="gname" placeholder="S Sathyaraj"  required>
+                    <input type="text" class="form-control" name="gname" placeholder="S Sathyaraj">
                     </div>
 
                     <div class="col-6">
                     <label for="gaddress">Address :</label>
-                    <input type="text" class="form-control" name="gaddress" placeholder="117, Old Post Office Road, Vinayagapuram 02. Thirukkovil." required>
+                    <input type="text" class="form-control" name="gaddress" placeholder="117, Old Post Office Road, Vinayagapuram 02. Thirukkovil.">
                     </div>
 
                     <div class="col-3">
                     <label for="gphone">Phone No :</label>
-                    <input type="tel" class="form-control" maxlength="10" minlength="10" name="gphone" placeholder="0700000000"  required>
+                    <input type="tel" class="form-control" maxlength="10" minlength="10" name="gphone" placeholder="0700000000">
                     </div>
 
                 </div>
@@ -349,7 +463,7 @@ $description = "Online Examination Result Management System (ERMS)-SLGTI";
 
                     <div class="col-3">
                     <label for="grelation">Relationship :</label>
-                    <select name="grelation" class="custom-select" required>
+                    <select name="grelation" class="custom-select">
                     <option value=""> Choose Relationship</option>
                     <option value="mother"> Mother </option>
                     <option value="father"> Father </option>
@@ -364,7 +478,14 @@ $description = "Online Examination Result Management System (ERMS)-SLGTI";
                 <div class="row">
                      <div class="col-11 "></div>
                      <div class="col-1">
-                     <button type="submit" name="submit" class="btn btn-outline-success">Add</button>
+                     <?php
+                     if (isset($_GET['edit'])){
+                         echo '<button type="submit" name="update" class="btn btn-outline-success">Update</button>';
+                        }
+                        else{
+                            echo '<button type="submit" name="add" class="btn btn-outline-success">Add</button>';
+                        }
+                        ?>
                      </div>
                      </div>
                 <!-- 2nd row end -->
@@ -380,98 +501,3 @@ $description = "Online Examination Result Management System (ERMS)-SLGTI";
 
 </html>
 
-<!-- insert  start-->
-<?php
-if(
-    isset($_POST['submit'])  
-    && !empty($_POST['regno'])
-    && !empty($_POST['title'])
-    && !empty($_POST['fullname'])
-    && !empty($_POST['ini_name']) 
-    && !empty($_POST['gender']) 
-    && !empty($_POST['civil']) 
-    && !empty($_POST['email']) 
-    && !empty($_POST['nic']) 
-    && !empty($_POST['dob']) 
-    && !empty($_POST['phone']) 
-    && !empty($_POST['address']) 
-    && !empty($_POST['ds'])
-    && !empty($_POST['district']) 
-    && !empty($_POST['province']) 
-    && !empty($_POST['zip'])
-    && !empty($_POST['blood']) 
-    && !empty($_POST['gname'])
-    && !empty($_POST['gaddress']) 
-    && !empty($_POST['gphone']) 
-    && !empty($_POST['grelation'])
-)
-{
-    $regno = $_POST['regno'];
-    $title = $_POST['title'];
-    $fullname = $_POST['fullname'];
-    $ini_name = $_POST['ini_name'];
-    $gender = $_POST['gender'];
-    $civil = $_POST['civil'];
-    $email = $_POST['email'];
-    $nic = $_POST['nic'];
-    $dob = $_POST['dob'];
-    $phone = $_POST['phone'];
-    $address = $_POST['address'];
-    $ds = $_POST['ds'];
-    $district = $_POST['district'];
-    $province = $_POST['province'];
-    $zip = $_POST['zip'];
-    $blood = $_POST['blood'];
-    $gname = $_POST['gname'];
-    $gaddress = $_POST['gaddress'];
-    $gphone = $_POST['gphone'];
-    $grelation = $_POST['grelation'];
-
-    $sqlstudent = "INSERT INTO student (id, title, full_name, name_with_initials, gender, civil_status, email, nic, date_of_birth, 
-    phone_no, address, divisional_secretariat, district, province, zip_code, blood_group, guardian_name, guardian_address, guardian_phone_no,
-    guardian_relationship) VALUES 
-    ('$regno','$title','$fullname','$ini_name','$gender','$civil','$email','$nic','$dob','$phone',
-    '$address','$ds','$district','$province','$zip','$blood','$gname','$gaddress','$gphone','$grelation')";
-
-   if (mysqli_query($con,$sqlstudent)) 
-   {
-       echo "New record created successfully";
-    } else 
-    {
-        echo "Error: " . $sqlstudent . "<br>" . mysqli_error($con);
-    }
-}
-
-if(
-    isset($_POST['submit'])
-    && !empty($_POST['regno']) 
-    && !empty($_POST['cid'])
-    && !empty($_POST['ayear']) 
-    && !empty($_POST['mode']) 
-    && !empty($_POST['status']) 
-    && !empty($_POST['enrolldate']) 
-    && !empty($_POST['exitdate']))
-    {
-        echo "SUCCESS";
-        $regno = $_POST['regno'];
-        $cid=$_POST['cid'];
-        $ayear=$_POST['ayear'];
-        $mode=$_POST['mode'];
-        $status=$_POST['status'];
-        $enrolldate=$_POST['enrolldate'];
-        $exitdate=$_POST['exitdate'];
-
-          $sqlenroll = "INSERT INTO student_enroll (id, course_id, academic_year, course_mode, student_status,
-          enroll_date, exit_date) VALUES ('$regno','$cid','$ayear','$mode','$status','$enrolldate','$exitdate')";
-
-            if(mysqli_query($con,$sqlenroll))
-            {
-              echo "Record Insert Successfully";
-            }
-            else
-            {
-              echo "Error: " . $sqlenroll . "<br>" . mysqli_error($con);
-            }
-    }
-?>
-<!-- insert  end -->
