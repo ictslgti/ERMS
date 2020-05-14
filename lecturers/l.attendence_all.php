e93a4040550bbea877048e11baf280f10ec95972
JAWSI <irhamuljawsi@gmail.com>
2020-05-14 21:38:23 +0530

    add attendence

---
+++ lecturers/l.attendence_all.php
@@ -0,0 +1,248 @@
+<?php
+$title = ' ERMS | SLGTI(page Title)';
+$description = 'Online Examination Result  Management System (ERMS)-SLGTI';
+?>
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+    <?php include_once('.././head.php');
+    //include_once('../config.php');
+    ?>
+</head>
+
+<body>
+    <main class='page-content pt-2'>
+        <?php include_once('nav.php');
+        ?>
+        <div id='overlay' class='overlay'></div>
+        <div class='container-fluid p-5'>
+            <!-- #1 Insert Your Content-->
+
+
+
+            <div class="container">
+
+                <!-- 1st row start -->
+                <div class="row">
+                     <div class="col-sm">
+                        <div class="border border-primary rounded text-center">
+                            <h2>Student's Attendence | SLGTI</h2>
+                        </div>
+                    </div>
+                </div>
+                    <br>
+                <!-- 1st row end -->
+
+                <form method="POST" action="">
+                    <div class="row">
+                        <div class='col-7'>
+                            <div class='form-group col-md'>
+                                <ul class='nav nav-tabs'>
+                                    <li class='nav-item'>
+                                        <a class='nav-link active' href='./l.attendence_all.php'>ALL Students</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                
+                <div class="row">
+                    <div class="form-group col-md-12 table-responsive">
+                    <table class="table align-middle">
+                        <thead class='thead-light'>
+                            <tr>
+                                <th scope='col'>No.</th>
+                                <th scope='col'>Reg No.</th>
+                                <th scope='col'>1st Lec</th>
+                                <th scope='col'>2nd Lec</th>
+                                <th scope='col'>3rd Lec</th>
+                                <th scope='col'>4th Lec</th>
+                                <th scope='col'>5th Lec</th>
+                                <th scope='col'>6th Lec</th>
+                                <th scope='col'>7th Lec</th>
+                                <th scope='col'>8th Lec</th>
+                                <th scope='col'>9th Lec</th>
+                                <th scope='col'>10th Lec</th>
+                                <th scope='col'>11th Lec</th>
+                                <th scope='col'>12th Lec</th>
+                                <th scope='col'>13th Lec</th>
+                                <th scope='col'>14th Lec</th>
+                                <th scope='col'>15th Lec</th>
+                                <th scope='col'>16th Lec</th>
+                                <th scope='col'>17th Lec</th>
+                                <th scope='col'>18th Lec</th>
+                                <th scope='col'>19th Lec</th>
+                                <th scope='col'>20th Lec</th>
+                                <th scope='col'>Total Lec</th>
+                            </tr>
+                            <tr>
+                                <td scope='col'>01</td>
+                                <td scope='col'>2018slgtibit01</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>0</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>0</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>18</td>
+                            </tr>
+                            <tr>
+                                <td scope='col'>02</td>
+                                <td scope='col'>2018slgtibit02</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>0</td>
+                                <td scope='col'>0</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>0</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>0</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>0</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>15</td>
+                            </tr>
+                            <tr>
+                                <td scope='col'>03</td>
+                                <td scope='col'>2018slgtibit05</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>0</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>0</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>0</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>0</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>16</td>
+                            </tr>
+                            <tr>
+                                <td scope='col'>04</td>
+                                <td scope='col'>2018slgtibit06</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>0</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>0</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>0</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>0</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>16</td>
+                            </tr>
+                            <tr>
+                                <td scope='col'>05</td>
+                                <td scope='col'>2018slgtibit07</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>0</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>0</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>1</td>
+                                <td scope='col'>18</td>
+                            </tr>
+                            
+                            <tr>
+                                <td colspan="2">Total attendance</td>
+                                <td>05</td>
+                                <td>04</td>
+                                <td>01</td>
+                                <td>05</td>
+                                <td>04</td>
+                                <td>04</td>
+                                <td>04</td>
+                                <td>04</td>
+                                <td>05</td>
+                                <td>05</td>
+                                <td>00</td>
+                                <td>05</td>
+                                <td>05</td>
+                                <td>05</td>
+                                <td>03</td>
+                                <td>05</td>
+                                <td>05</td>
+                                <td>04</td>
+                                <td>05</td>
+                                <td>05</td>
+                                <td></td>
+                            </tr>
+                        </thead>
+                    <tbody>
+                     
+                    </tbody>
+                </table>
+                    </div>
+                </div>
+
+
+
+             <!-- #1 Insert Your Content" -->
+        </div>
+    </main>
+
+    <?php include_once("../script.php");
+    ?>
+</body>
+
+</html>
