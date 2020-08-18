98b5c3cc893ebabb59af7f10a7866d7cd5b95e9d
JAWSI <irhamuljawsi@gmail.com>
2020-08-18 13:57:45 +0530

    database connect

--- lecturers/l.attendance_all.php
+++ lecturers/l.attendance_all.php
@@ -7,7 +7,7 @@ $description = 'Online Examination Result  Management System (ERMS)-SLGTI';
 
 <head>
     <?php include_once('.././head.php');
-    //include_once('../config.php');
+    include_once('../config.php');
     ?>
 </head>
 
