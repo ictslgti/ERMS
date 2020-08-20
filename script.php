487aedaf83e46b32502b52f9a72e17988d0259e7
K-avin <kabilraj050@gmail.com>
2020-08-20 15:21:06 +0530

    scripts-dash

--- script.php
+++ script.php
@@ -8,3 +8,24 @@
 
 
  <script src="js/main.js"></script>
+
+ <!-- kavi scripts -->
+ <!-- Dashboard script -->
+ <!-- Bootstrap core JavaScript-->
+ <script src="dashboard/vendor/jquery/jquery.min.js"></script>
+  <script src="dashboard/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="dashboard/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="dashboard/js/sb-admin-2.min.js"></script>
+
+  <!-- Page level plugins -->
+  <script src="dashboard/vendor/chart.js/Chart.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  <script src="dashboard/js/demo/chart-area-demo.js"></script>
+  <script src="dashboard/js/demo/chart-pie-demo.js"></script>
+ <!-- Dashboard script end -->
+ <!-- kavi scripts end -->
