2be1151b0748801e14f87b1650a1da96cca335f2
K-avin <kabilraj050@gmail.com>
2020-08-27 12:06:59 +0530

    update overwrite

--- head.php
+++ head.php
@@ -13,14 +13,4 @@
 <link rel="stylesheet" href="css/sidebar-themes.css">
 <link rel="shortcut icon" type="image/png" href="img/favicon.png" />
 
-
-  <!-- kavi links start -->
-  <!-- Custom fonts-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-  <!-- Custom styles-->
-  <link href="dashboard/css/sb-admin-2.min.css" rel="stylesheet">
-
-  <!-- MD BOOSTRUP -->
-  <!-- kavi links end -->
+  
