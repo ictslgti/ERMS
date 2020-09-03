37df0b75eab3b5673b40b5366e3d59392368a575
K-avin <kabilraj050@gmail.com>
2020-09-03 11:05:59 +0530

    ADD chart alert MODEL

--- students/attendance_month.php
+++ students/attendance_month.php
@@ -35,12 +35,17 @@ $description = 'Online Examination Result  Management System (ERMS)-SLGTI';
                                         <div class="row">
                                             <div class="col-3">
                                                 <div class="dropdown">
-                                                    <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" value="show details" name="show_date">
+                                                    <button class="btn btn-light dropdown-toggle" type="button"
+                                                        id="dropdownMenuButton" data-toggle="dropdown"
+                                                        aria-haspopup="true" aria-expanded="false" value="show details"
+                                                        name="show_date">
                                                         Attendance Review
                                                     </button>
                                                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                                        <a class="dropdown-item" href="attendance_month.php">Month-wise</a>
-                                                        <a class="dropdown-item" href="attendance_semester.php">Semester-wise</a>
+                                                        <a class="dropdown-item"
+                                                            href="attendance_month.php">Month-wise</a>
+                                                        <a class="dropdown-item"
+                                                            href="attendance_semester.php">Semester-wise</a>
                                                     </div>
                                                 </div>
 
@@ -48,7 +53,8 @@ $description = 'Online Examination Result  Management System (ERMS)-SLGTI';
 
                                             <div class="col-3">
                                                 <label for="month"> Month: </label>
-                                                <select class="custom-select" name="month" id="month" data-live-search="true" data-width="100%" value="" required>
+                                                <select class="custom-select" name="month" id="month"
+                                                    data-live-search="true" data-width="100%" value="" required>
                                                     <option value=""> Choose</option>
                                                     <option value=""> January </option>
                                                     <option value=""> February </option>
@@ -68,27 +74,51 @@ $description = 'Online Examination Result  Management System (ERMS)-SLGTI';
                                             <div class="col-3"></div>
 
                                             <div class="col-3" align="right">
-                                                <button type="button" id="add_button" class="btn btn-info btn-sm">Chart</button>
+                                                <button type="button" class="btn btn-primary" data-toggle="modal"
+                                                    data-target="#staticBackdrop">
+                                                    Chart
+                                                </button>
+                                                <div class="modal fade" id="staticBackdrop" data-backdrop="static"
+                                                    data-keyboard="false" tabindex="-1"
+                                                    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                                    <div class="modal-dialog modal-dialog-centered modal-lg">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <h5 class="modal-title" id="staticBackdropLabel">
+                                                                    Chart</h5>
+                                                                <button type="button" class="close" data-dismiss="modal"
+                                                                    aria-label="Close">
+                                                                    <span aria-hidden="true">&times;</span>
+                                                                </button>
+                                                            </div>
+                                                            <div class="modal-body col-md-6">
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                                <button type="button" class="btn btn-secondary"
+                                                                    data-dismiss="modal">Close</button>                                                               
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </div>
 
-                                    <div class="card-body">
+                                            <div class="card-body">
 
-                                        <div class="table-responsive">
+                                                <div class="table-responsive">
 
-                                            <span id="message_operation"></span>
-                                            <table class="table table-striped table-bordered" id="attendance_table">
-                                                <thead>
-                                                    <tr>
-                                                        <th>Course</th>
-                                                        <th>Take session</th>
-                                                        <th>points</th>
-                                                        <th>remarks</th>
-                                                        <th>Attendance Date</th>
-                                                    </tr>
+                                                    <span id="message_operation"></span>
+                                                    <table class="table table-striped table-bordered"
+                                                        id="attendance_table">
+                                                        <thead>
+                                                            <tr>
+                                                                <th>Course</th>
+                                                                <th>Take session</th>
+                                                                <th>points</th>
+                                                                <th>remarks</th>
+                                                                <th>Attendance Date</th>
+                                                            </tr>
 
-                                                    <?php
+                                                            <?php
                                                     if (isset($_POST['show_date'])) {
                                                         $dat = $_POST['prese_date'];
                                                         $sql = "SELECT 
@@ -105,35 +135,35 @@ $description = 'Online Examination Result  Management System (ERMS)-SLGTI';
                                                     $result = mysqli_query($con, $sql);
                                                     while ($row = mysqli_fetch_assoc($result)) {
                                                     ?>
-                                                        <tr>
-                                                            <td scope='col'>
-                                                                <?php echo $row['Course']; ?>
-                                                            </td>
-                                                            <td scope='col'>
-                                                                <?php echo $row['student_id']; ?>
-                                                            </td>
-                                                            <td scope='col'>
-                                                                <?php echo $row['student_batch']; ?>
-                                                            </td>
-                                                            <td scope='col'>
-                                                                <?php echo $row['status']; ?>
-                                                            </td>
-                                                            <td scope='col'>
-                                                                <?php echo $row['attendance_date']; ?>
-                                                            </td>
-                                                        <?php
+                                                            <tr>
+                                                                <td scope='col'>
+                                                                    <?php echo $row['Course']; ?>
+                                                                </td>
+                                                                <td scope='col'>
+                                                                    <?php echo $row['student_id']; ?>
+                                                                </td>
+                                                                <td scope='col'>
+                                                                    <?php echo $row['student_batch']; ?>
+                                                                </td>
+                                                                <td scope='col'>
+                                                                    <?php echo $row['status']; ?>
+                                                                </td>
+                                                                <td scope='col'>
+                                                                    <?php echo $row['attendance_date']; ?>
+                                                                </td>
+                                                                <?php
                                                     }
                                                         ?>
-                                                        </tr>
-                                                </thead>
-                                                <tbody>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
 
-                                                </tbody>
-                                            </table>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
 
 </body>
 
