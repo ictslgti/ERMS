db5153d468c25bff360e2a828082c14aff6a39d8
JAWSI <irhamuljawsi@gmail.com>
2020-09-04 12:05:08 +0530

    update

--- students/attendance.php
+++ students/attendance.php
@@ -72,9 +72,7 @@ $description = 'Online Examination Result  Management System (ERMS)-SLGTI';
 
                                             <div class="col-3"></div>
 
-                                            <div class="col-3" align="right">
-                                                <button type="button" id="add_button" class="btn btn-info btn-sm">Chart</button>
-                                            </div>
+                                            
                                         </div>
                                     </div>
 
