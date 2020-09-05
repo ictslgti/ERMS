a863bdb406058efbb24790dd9fbe663c26ca01aa
K-O-G-I-L-A <55224732+K-O-G-I-L-A@users.noreply.github.com>
2020-09-05 21:12:29 +0530

    Update courses.php

--- admin/courses.php
+++ admin/courses.php
@@ -14,175 +14,141 @@ $description = "Online Examination Result  Management System (ERMS)-SLGTI";
 
 <body>
     <div class="page-wrapper toggled bg2 border-radius-on light-theme">
-        
-            <?php include_once("nav.php"); ?>
-       
-        <!-- <main class="page-content pt-2"> -->
-            <!-- 1st row start -->
 
-         <div class="container"> 
-                  <!-- delete -->
-<?php
-if(isset($_GET['delete'])){
-    $id = $_GET['delete'];
-    $sql = "DELETE FROM `courses` WHERE `courses`.`code` = '$id'";
-    if(mysqli_query($con,$sql)){
-        echo "
+        <?php include_once("nav.php"); ?>
+
+        <!-- <main class="page-content pt-2"> -->
+        <!-- 1st row start -->
+
+        <div class="container">
+            <!-- delete -->
+            <?php
+            if (isset($_GET['delete'])) {
+                $id = $_GET['delete'];
+                $sql = "DELETE FROM `courses` WHERE `courses`.`id` = '$id'";
+                if (mysqli_query($con, $sql)) {
+                    echo "
        <div class='alert alert-success' role='alert'>
        delete success fully 
        <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
            <span aria-hidden='true'>&times;</span>
         </button>
       </div>";
-    }else{
-        echo "Error: " . $sql . "<br>" . mysqli_error($con);
-    }
-}
-?>
-<!-- delete -->
-             <br>
-         <form action=""> 
-            <div class="card  mb-3" >
-                <div class="card-header ">
-                <div class="row">
-                <div class="col"><h4>Course</h4></div>
-                <div class="col-auto">
-                <a href="course.php" class="btn btn-outline-primary">New</a>
-                </div>
+                } else {
+                    echo "Error: " . $sql . "<br>" . mysqli_error($con);
+                }
+            }
+            ?>
+            <!-- delete -->
+            <br>
+            <form action="">
+                <div class="card  mb-3">
+                    <div class="card-header ">
+                        <div class="row">
+                            <div class="col">
+                                <h4>Course</h4>
+                            </div>
+                            <div class="col-auto">
+                                <a href="course.php" class="btn btn-outline-primary">New</a>
+                            </div>
 
-                </div>
-                </div>
-                
-                   
+                        </div>
+                    </div>
+
+
                     <div class="card-body ">
-                    <form method="POST">
-                <div class="row">
-                    <div class="form-group col-md-12">
-                    <div class="table-responsive">
-                    
-                    <table class='table align-middle'>
-                        <thead class='bg-primary text-light'>
-                            <tr class="h5">
-                                <th scope='col'>code</th>
-                                <th scope='col'>name</th>
-                                <th scope='col'>Level</th>
-                                <th scope='col'>Department</th>
-                                <th scope='col'>Options</th>
-                            </tr>
-                            
-                        </thead>
-                        <tbody>
-                        
-            
+                        <form method="POST">
+                            <div class="row">
+                                <div class="form-group col-md-12">
+                                    <div class="table-responsive">
 
-<?php
+                                        <table class='table align-middle'>
+                                            <thead class='bg-primary text-light'>
+                                                <tr>
+                                                    <th scope='col'>code</th>
+                                                    <th scope='col'>name</th>
+                                                    <th scope='col'>Level</th>
+                                                    <th scope='col'>Department</th>
+                                                    <th scope='col'></th>
+                                                </tr>
 
-if(isset($_GET['view']))
-{
-    $view=$_GET['view'];
-    $sql = "select * from courses where department=(select department_code from departments where id=$view);";
-}
-else
-{
-    $sql = 'SELECT * FROM `courses`';
-}
-$result = mysqli_query($con,$sql);
-if(mysqli_num_rows($result)>0){
-    while($row = mysqli_fetch_assoc($result)){
-        echo '<tr>
-            <td>',$row['code'],'</td>
-            <td>',$row['name'],'</td>
-            <td>',$row['NVQ_Level'],'</td>
-            <td>',$row['department'],'</td>
+                                            </thead>
+                                            <tbody>
+
+
+
+                                                <?php
+
+                                                if (isset($_GET['view'])) {
+                                                    $view = $_GET['view'];
+                                                    $sql = "SELECT * FROM `courses` where department_code='$view'";
+                                                } else {
+                                                    $sql = 'SELECT * FROM `courses`';
+                                                }
+                                                $result = mysqli_query($con, $sql);
+                                                if (mysqli_num_rows($result) > 0) {
+                                                    while ($row = mysqli_fetch_assoc($result)) {
+                                                        echo '<tr>
+            <td>', $row['code'], '</td>
+            <td>', $row['name'], '</td>
+            <td>', $row['NVQ_level'], '</td>
+            <td>', $row['department_code'], '</td>
             
             
             <td>
+            <div class="row">
+            <div class="col"></div>
+            <div class="col-auto">
             <div class="btn-group btn-sm" role="group" aria-label="Basic example">
-            <a href="course.php?edit=',$row['code'],'" class="btn btn-warning" > <img src="https://img.icons8.com/android/18/000000/edit.png"/> </a> 
-            <a href="?delete=  ',$row['code'],'" class="btn btn-danger"> <img src="https://img.icons8.com/windows/18/000000/delete-forever.png"/> </a>
-            <a href="modules.php?view=',$row['code'],'" class="btn btn-success"><b>view modules</b> </a>
+            <a href="course.php?edit=', $row['id'], '" class="btn btn-warning" > <img src="https://img.icons8.com/android/18/000000/edit.png"/> </a> 
+            <a href="?delete=', $row['id'], '" class="btn btn-danger"><img src="https://img.icons8.com/windows/18/000000/delete-forever.png"/> </a>
+            <a href="modules.php?view=', $row['code'], '" class="btn btn-success"><b>modules</b> </a>
+            </div>
+            </div>
           </div>
             </td>
         </tr>';
-    }
-}
-else{
-    echo 'no rows';
-}
-?>  
+                                                    }
+                                                } else {
+                                                    echo 'no rows';
+                                                }
+                                                ?>
 
-</tbody>
-                    </table>
-                    
+                                            </tbody>
+                                        </table>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+
                     </div>
-                    </div>
+                    <br>
+                    <!-- 2 row end -->
                 </div>
-                </form>
-                    
-                     </div>
-                     <br>
-                     <!-- 2 row end -->
-                    </div>
-                   
-                   
-                
+
+
+
                 <!-- 1st row end -->
-            <!-- <div id="overlay" class="overlay"></div>
+                <!-- <div id="overlay" class="overlay"></div>
             <div class="container-fluid p-5"> -->
                 <!-- #1 Insert Your Content-->
                 <!-- <div class="container"> -->
-                    <!-- 2 row start -->
-                    
-                     
-                     
-                     </div>
-</form>
-                    
+                <!-- 2 row start -->
 
 
-                </div>
-                <!-- #1 Insert Your Content" -->
-            </div>
-        </main>
+
+        </div>
+        </form>
+
+
+
+    </div>
+    <!-- #1 Insert Your Content" -->
+    </div>
+    </main>
     </div>
     <?php include_once("../script.php"); ?>
 </body>
 
 </html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
