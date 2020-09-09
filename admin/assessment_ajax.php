24eef6d8f61a34f9e72a795085b78a28c552573e
Reggy Kodvin <55224697+kodvinkod@users.noreply.github.com>
2020-09-09 14:08:07 +0530

    Add files via upload

---
+++ admin/assessment_ajax.php
@@ -0,0 +1,49 @@
+<style>
+    table
+     {
+      width: 100%;
+      border: 1px solid skyblue;
+    }
+    tr,th{
+      border: 1px solid skyblue;
+    }
+  </style>
+
+<?php
+include_once("../config.php");
+      
+      if (isset($_POST["action"])) {
+        $output = '';
+        if ($_POST["action"] == "department") {
+          $query = "SELECT * FROM courses WHERE department_code = '" . $_POST["query"] . "'";
+          $result = mysqli_query($con, $query);
+          $output .= '<option value="" disabled selected>Select course </option>';
+          while ($row = mysqli_fetch_array($result)) {
+            $output .= '<option value="' . $row["code"] . '">' . $row["name"] . '</option>';
+          }
+        }
+        else if ($_POST["action"] == "course") {
+          $query = "SELECT * FROM modules WHERE course_code = '" . $_POST["query"] . "'";
+          $result = mysqli_query($con, $query);
+          $output .= '<option value="" disabled selected>Select module</option>';
+          while ($row = mysqli_fetch_array($result)) {
+            $output .= '<option value="' . $row["code"] . '">' . $row["code"] . '</option>';
+          }
+        }
+
+        else if ($_POST["action"] == "module") {
+          $query = "SELECT * FROM `assessments` WHERE module ='" . $_POST["query"] . "'";
+          $result = mysqli_query($con, $query);
+          $output .= '<table>';
+          $output .= '<tr> <th> Name </th> <th> Percentage </th> </tr>';
+          while ($row = mysqli_fetch_array($result)) {
+            $output .= '<tr>';
+            $output .= '<td>'.$row['name'].'</td>';
+            $output .= '<td>'.$row['percentage'].'</td>';
+            $output .= '</tr>';
+          }
+          $output .= '</table>';
+        }
+        echo $output;
+      }
+      ?>
