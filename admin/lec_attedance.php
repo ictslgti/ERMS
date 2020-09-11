8fb3b692eb11b1c0471807755c14355821848340
subashiny97 <subashiny97@gmail.com>
2020-09-11 11:45:36 +0530

    #edit

--- admin/lec_attedance.php
+++ admin/lec_attedance.php
@@ -13,115 +13,71 @@ $description = "Online Examination Result  Management System (ERMS)-SLGTI";
 <body>
     <div class="page-wrapper toggled bg2 border-radius-on light-theme">
         <?php include_once("nav.php"); ?>
-         <div class="container">
-          <!-- content -->
-          <br>
-          <div class="card border-secondary mb-3">
-            <div class="card-header ">
-            <div class="row">
-            <div class="col-sm">
-            <h1>Attendance</h1>
-            </div>
-            </div>
-            </div>
-            <div class="card-body text-dark">
-
-            <div class="row">
-                            <div class="col-sm">
-                       <br>
-                     <div class="form-group">
-                     <div class="input-group input-group-sm mb-3">
-  
-                        <select name="NVQ_Level"class="custom-select" id="inputGroupSelect01"id="validationCustom04" required>
-                        <option selected disabled value="">Choose course </option>
-                            <option value="NVQ-04">NVQ Level 04</option>
-                            <option value="NVQ-05 Bridging">Bridging Level 05</option>
-                            <option value="NVQ-05 "> NVQ Level 05</option>
-                            
-                        </select>
+        <div class="container">
+            <!-- content -->
+            <br>
+            <div class="card border-secondary mb-3">
+                <div class="card-header ">
+                    <div class="row">
+                        <div class="col-sm">
+                            <h1>Attendance</h1>
                         </div>
-                         </div>
-                     </div>
-
-
-                     <div class="col-sm">
-                     NVQ Level  <br>
-                     <div class="form-group">
-                     <div class="input-group input-group-sm mb-3">
-  
-                        <select name="NVQ_Level"class="custom-select" id="inputGroupSelect01"id="validationCustom04" required>
-                        <option selected disabled value="">Choose module </option>
-                            <option value="NVQ-04">Web programming</option>
-                            <option value="NVQ-05 Bridging">Database Systems 1 </option>
-                            <option value="NVQ-05">Software Testing</option>
-                            <option value="NVQ-05">Software Programming</option>
-                            <option value="NVQ-05">Local Area network</option>
-                            <option value="NVQ-05 Bridging">Basic computer Application</option>
-                            <option value="NVQ-05">Database systems 2</option>
-                            <option value="NVQ-05 Bridging">Fundamentals Of Graphic Design </option>
-                            <option value="NVQ-05">Graphic Design</option>
-                            <option value="NVQ-05">Planning And Scheduling Work At Workplace</option>
-                            <option value="NVQ-05 Bridging">Static Web Page Desiging</option>
-                            <option value="NVQ-05">System Anaysis And Design</option>
-                            
-
-                        </select>
+                    </div>
+                </div>
+                <div class="card-body text-dark">
+                    <!-- first row -->
+                    <div class="row">
+                        <div class="col">
+                            <p>Date -: 2020-10-08</p>
+                            <p>Time -: 12.30</p>
+                            <p>Lecture -: Y.Achchuthan</p>
                         </div>
-                         </div>
-                     </div>
-                </div> 
-
-                <div class="row">
-                            <div class="col-sm">
-                       <br>
-                     <div class="form-group">
-                     <div class="input-group input-group-sm mb-3">
-  
-                        <select name="NVQ_Level"class="custom-select" id="inputGroupSelect01"id="validationCustom04" required>
-                        <option selected disabled value="">Choose course </option>
-                            <option value="NVQ-04">NVQ Level 04</option>
-                            <option value="NVQ-05 Bridging">Bridging Level 05</option>
-                            <option value="NVQ-05 "> NVQ Level 05</option>
-                            
-                        </select>
+                        <div class="col-auto">
+                            <p>module -: M07</p>
+                            <p>Batch -: 05</p>
+                            <p></p>
                         </div>
-                         </div>
-                     </div>
+                    </div>
+                    <!-- first row end -->
+                    <hr>
+                    <!-- seond row -->
+                    <div class="row">
+                        <div class="col"><h6>Name</h6></div>
+                        <div class="col-auto"><h6>status</h6></div>
+                    </div>
 
+             <hr>
+                    <div class="row">
+                        <div class="col">Sithamparanathan sumanan</div>
+                        <div class="col-auto"><input type="checkbox"></div>
+                    </div>
+<hr>
+                    <div class="row">
+                        <div class="col">Sathijaseelan sathursan</div>
+                        <div class="col-auto"><input type="checkbox"></div>
+                    </div>
+<hr>
+                    <div class="row">
+                        <div class="col">Ramachandran Kogilavani</div>
+                        <div class="col-auto"><input type="checkbox"></div>
+                    </div>
+<hr>
+                    <div class="row">
+                        <div class="col">Balasubramaniyan kiriya</div>
+                        <div class="col-auto"><input type="checkbox"></div>
+                    </div>
+                    <!-- second end -->
 
-                     <div class="col-sm">
-                     NVQ Level  <br>
-                     <div class="form-group">
-                     <div class="input-group input-group-sm mb-3">
-  
-                        <select name="NVQ_Level"class="custom-select" id="inputGroupSelect01"id="validationCustom04" required>
-                        <option selected disabled value="">Choose module </option>
-                            <option value="NVQ-04">Web programming</option>
-                            <option value="NVQ-05 Bridging">Database Systems 1 </option>
-                            <option value="NVQ-05">Software Testing</option>
-                            <option value="NVQ-05">Software Programming</option>
-                            <option value="NVQ-05">Local Area network</option>
-                            <option value="NVQ-05 Bridging">Basic computer Application</option>
-                            <option value="NVQ-05">Database systems 2</option>
-                            <option value="NVQ-05 Bridging">Fundamentals Of Graphic Design </option>
-                            <option value="NVQ-05">Graphic Design</option>
-                            <option value="NVQ-05">Planning And Scheduling Work At Workplace</option>
-                            <option value="NVQ-05 Bridging">Static Web Page Desiging</option>
-                            <option value="NVQ-05">System Anaysis And Design</option>
-                            
-
-                        </select>
-                        </div>
-                         </div>
-                     </div>
-                </div> 
-
-
-            </div>
-            <div class="card-footer bg-transparent border-secondary">Footer</div>
+                </div>
+                <div class="card-footer bg-transparent border-secondary">
+                    <div class="row">
+                        <div class="col" ><input type="submit" value="add" class="btn btn-outline-primary"></div>
+                        <div class="col-auto">click all select<input type="checkbox"></div>
+                    </div>
+                </div>
             </div>
 
-           <!-- content -->
+            <!-- content -->
         </div>
     </div>
     <?php include_once("../script.php"); ?>
