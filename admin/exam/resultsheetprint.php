56d5809157d0e48f0cb05daa33d013fe6ac55904
KishokPuvalenthiran98 <kishokpuva98@gmail.com>
2020-09-18 12:37:40 +0530

    add file

--- admin/exam/resultsheetprint.php
+++ admin/exam/resultsheetprint.php
@@ -9,7 +9,6 @@ $description = "Online Examination Result  Management System (ERMS)-SLGTI";
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="print.css" media="print">
     <?php include_once("../../head.php"); ?>
     <?php include_once("../../config.php"); ?>
     <style>
@@ -23,9 +22,11 @@ $description = "Online Examination Result  Management System (ERMS)-SLGTI";
             border: 1px solid black;
         }
     </style>
+
 </head>
 
 <body>
+
 
     <?php
     $d_id = null;
@@ -44,11 +45,12 @@ $description = "Online Examination Result  Management System (ERMS)-SLGTI";
     }
     ?>
     <div class="page-wrapper toggled bg2 border-radius-on light-theme">
+
         <?php include_once("../nav.php"); ?>
         <!-- id array -->
         <?php
 
-        $sql = mysqli_query($con, "SELECT DISTINCT student_id FROM exam_result");
+        $sql = mysqli_query($con, "SELECT DISTINCT student_id FROM exams_result");
 
         while ($row = mysqli_fetch_array($sql)) {
             $s_ids[] = $row['student_id'];
@@ -62,13 +64,20 @@ $description = "Online Examination Result  Management System (ERMS)-SLGTI";
 
         <!-- subject array -->
         <?php
-        $sql1 = mysqli_query($con, "SELECT DISTINCT module_name FROM exam_result");
+        $sql1 = mysqli_query($con, "SELECT DISTINCT module FROM exams_result");
 
         while ($row1 = mysqli_fetch_array($sql1)) {
-            $subjects[] = $row1['module_name'];
+            $subjects[] = $row1['module'];
         }
         ?>
         <!-- subject array  -->
+        <div>
+            <p></p>
+        </div>
+        <div>
+            <p style="text-align: center; font-size: 30px;"> SLGTI-KILLINOCHI</p>
+
+        </div>
 
         <table class="table">
             <thead class="thead-dark">
@@ -108,11 +117,14 @@ $description = "Online Examination Result  Management System (ERMS)-SLGTI";
                         for ($s = 0; $s < $length1; $s++) {
                             $s_ids_val = $s_ids[$i];
                             $subjects_val = $subjects[$s];
-                            $sql3 = "select  status from student s inner join exam_result e on s.id=e.student_id and student_id='$s_ids_val' and module_name='$subjects_val';";
+                            $sql3 = "select e.marks from student_enroll s inner join exams_result e on s.id=e.student_id and e.student_id='$s_ids_val' and e.module='$subjects_val'";
                             $result = mysqli_query($con, $sql3);
                             if (mysqli_num_rows($result) > 0) {
                                 if ($row = mysqli_fetch_assoc($result)) {
-                                    echo '<td>', $row['status'], '</td>';
+
+
+                                    // echo $row['marks'];
+                                    echo '<td>', $row['marks'], '</td>';
                                 }
                             }
                         }
