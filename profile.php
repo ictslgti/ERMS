39e9866546b169e26b34c9147e6652162a59fa70
K-avin <kabilraj050@gmail.com>
2020-09-18 15:09:29 +0530

    user profile

--- profile.php
+++ profile.php
@@ -20,29 +20,102 @@ $description = "Online Examination Result  Management System (ERMS)-SLGTI";
         <div id="overlay" class="overlay"></div>
         <!-- <div class="container-fluid p-5"> -->
         <!-- #1 Insert Your Content-->
-        <div class="card text-center">
-            <div class="card-header">
-                Profile
+        <div class="container">
+            <div class="avatar mx-auto white text-center"><img
+                    src="https://mdbootstrap.com/img/Photos/Avatars/img%20(20).jpg" alt="avatar mx-auto white"
+                    class="rounded-circle img-fluid">
+            </div>
+
+            <div class="card-body">
+                <h4 class="card-title mt-1 text-center">Anna Aston</h4>
+                <h4 class="card-title mt-1 text-center"><button type="button" class="btn btn-primary" data-toggle="modal"
+                        data-target="#staticBackdrop">Edit profile <i class="fas fa-edit"></i></button></h4>
+
+                        
+
             </div>
             <div class="card-body">
-                <img src="https://api.naiglobal.com/api/userimages?userId=697&imageType=profile"
-                    class="rounded float-center" style="width: 10vw; height: 10vw; " alt="...">
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-            <div class="jumbotron">
-                <h1 class="display-4">Kavi</h1>
-                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra
-                    attention to featured content or information.</p>
-                <hr class="my-4">
-                <p>It uses utility classes for typography and spacing to space content out within the larger container.
-                </p>
-                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-            </div>
-            <div class="card-footer text-muted">
-                2 days ago
+                <div class="row">
+                <table class="table center col-1">
+                    <table class="table col-10">
+                        <tr>
+                            <th scope="row">Username</th>
+                            <td>Mariya</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">E-mail*</th>
+                            <td>Mariya@gmail.com</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Course</th>
+                            <td>NVQ L5 (2018-2020)</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Phone</th>
+                            <td>0773569868</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Bithday</th>
+                            <td>1998.02.15</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Phone</th>
+                            <td>0773569868</td>
+                        </tr>
+                    </table>
+                    <table class="table center col-1">
+                </div>
             </div>
         </div>
+        </div>
+
+        </div>
+        <!-- Modal -->
+        <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1"
+                    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered modal-md">
+                        <form method="post">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="staticBackdropLabel">Reset password</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" id="inputAddress2" name="number"
+                                            placeholder="Register No / Nic No">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" id="inputAddress2" name="user_name"
+                                            placeholder="User name">
+                                    </div>
+                                    <!-- <div class="form-group">
+                                            <input type="email" class="form-control" id="inputAddress" name="email"
+                                                placeholder="Email">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" id="inputAddress2" name="Verify"
+                                                placeholder="Verification code">
+                                        </div> -->
+                                    <div class="form-row">
+                                        <div class="form-group col-md-6">
+                                            <input type="password" class="form-control" id="" placeholder="New Password"
+                                                name="new_password">
+                                        </div>
+                                        <div class="form-group col-md-6">
+                                            <input type="password" class="form-control" id=""
+                                                placeholder="Conform Password" name="c_password">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-primary" name="forgot">Reset</button>
+                                </div>
+                            </div>
+                        </form>
         <!-- #1 Insert Your Content" -->
         <!-- </div> -->
     </main>
