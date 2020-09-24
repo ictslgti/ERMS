21bfe3168c75d79b7c83d8ef82665771934289ac
Nufailniyas <Nufailniyas98@gmail.com>
2020-09-24 13:09:29 +0530

    session

--- lecturers/attendance_month.php
+++ lecturers/attendance_month.php
@@ -1,4 +1,10 @@
 <?php
+// session_start();
+// if (!isset($_SESSION['username'])) {
+//     header('Location: .././index.php');
+// }
+?>
+<?php
 $title = ' ERMS | SLGTI( Attendance)';
 $description = 'Online Examination Result  Management System (ERMS)-SLGTI';
 ?>
