28de5cb22052d9e23cd27269d36577b6a385c20c
KishokPuvalenthiran98 <kishokpuva98@gmail.com>
2020-09-24 15:20:31 +0530

    add file

--- admin/exam/resultsheetprint.php
+++ admin/exam/resultsheetprint.php
@@ -9,7 +9,7 @@ $description = "Online Examination Result  Management System (ERMS)-SLGTI";
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="print.css">
+    <link rel="stylesheet" type="text/css" href="print.css" media="print">
     <?php include_once("../../head.php"); ?>
     <?php include_once("../../config.php"); ?>
     <style>
