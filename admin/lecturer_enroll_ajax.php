c19921da18f657f6352be1faa9ca7d88039c6cb7
sumanan24 <sumanan24@gmail.com>
2020-09-24 13:39:42 +0530

    #result

--- admin/lecturer_enroll_ajax.php
+++ admin/lecturer_enroll_ajax.php
@@ -58,14 +58,17 @@ if (isset($_GET['courses'])) {
   if (isset($_GET['module'])) {
     $output = '';
      $_GET['module'];
-    $query = "select * from lecturer_enroll where module='".$_GET['module']."';";
+    // $query = " select (select first_name from lecturer where id in(select lecturer from lecturer_enroll)) as first_name,(select last_name from lecturer where id in(select lecturer from lecturer_enroll)) as last_name, (select  batch_no from batches where id in 
+    // (select batch from lecturer_enroll)) as batch from lecturer_enroll where module='".$_GET['module']."';";
+    $query = "select lecturer.first_name,lecturer.last_name,batches.batch_no from lecturer inner join lecturer_enroll on 
+    lecturer.id=lecturer_enroll.lecturer inner join batches on batches.id=lecturer_enroll.batch and lecturer_enroll.module='".$_GET['module']."'";
     $result = mysqli_query($con, $query);
     $output .= '<div class=row><div class="col-3"></div><div class="col-6"><table>';
     $output .= '<tr> <th> Lecturer name </th> <th> Batch </th> </tr>';
     while ($row = mysqli_fetch_array($result)) {
       $output .= '<tr>';
-      $output .= '<td>' . $row['lecturer'] . '</td>';
-      $output .= '<td>' . $row['batch'] . '</td>';
+      $output .= '<td>' . $row['first_name'] .$row['last_name'].'</td>';
+      $output .= '<td>' . $row['batch_no'] . '</td>';
       $output .= '</tr>';
     }
     $output .= '</table></div><div class="col-3"></div></div>';
