83886f1d23925b007d336cc29c7652646c5cf01e
K-O-G-I-L-A <55224732+K-O-G-I-L-A@users.noreply.github.com>
2020-09-28 00:01:55 +0530

    Add files via upload

---
+++ lecturers/courses.php
@@ -0,0 +1,154 @@
+<?php
+$title = "Courses | Online Examination Result Management System | SLGTI";
+$description = "Online Examination Result  Management System (ERMS)-SLGTI";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include_once("./head.php"); ?>
+    <?php include_once("../config.php"); ?>
+
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+
+<body>
+    <div class="page-wrapper toggled bg2 border-radius-on light-theme">
+
+        <?php include_once("nav.php"); ?>
+
+        <!-- <main class="page-content pt-2"> -->
+        <!-- 1st row start -->
+
+        <div class="container">
+            <!-- delete -->
+            <?php
+            if (isset($_GET['delete'])) {
+                $id = $_GET['delete'];
+                $sql = "DELETE FROM `courses` WHERE `courses`.`id` = '$id'";
+                if (mysqli_query($con, $sql)) {
+                    echo "
+       <div class='alert alert-success' role='alert'>
+       delete success fully 
+       <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+           <span aria-hidden='true'>&times;</span>
+        </button>
+      </div>";
+                } else {
+                    echo "Error: " . $sql . "<br>" . mysqli_error($con);
+                }
+            }
+            ?>
+            <!-- delete -->
+            <br>
+            <form action="">
+                <div class="card  mb-3">
+                    <div class="card-header ">
+                        <div class="row">
+                            <div class="col">
+                                <h4>Course</h4>
+                            </div>
+                            <div class="col-auto">
+                                <a href="course.php" class="btn btn-outline-primary">New</a>
+                            </div>
+
+                        </div>
+                    </div>
+
+
+                    <div class="card-body ">
+                        <form method="POST">
+                            <div class="row">
+                                <div class="form-group col-md-12">
+                                    <div class="table-responsive">
+
+                                        <table class='table align-middle'>
+                                            <thead class='bg-primary text-light'>
+                                                <tr>
+                                                    <th scope='col'>code</th>
+                                                    <th scope='col'>name</th>
+                                                    <th scope='col'>Level</th>
+                                                    <th scope='col'>Department</th>
+                                                    <th scope='col'></th>
+                                                </tr>
+
+                                            </thead>
+                                            <tbody>
+
+
+
+                                                <?php
+
+                                                if (isset($_GET['view'])) {
+                                                    $view = $_GET['view'];
+                                                    $sql = "SELECT * FROM `courses` where department_code='$view'";
+                                                } else {
+                                                    $sql = 'SELECT * FROM `courses`';
+                                                }
+                                                $result = mysqli_query($con, $sql);
+                                                if (mysqli_num_rows($result) > 0) {
+                                                    while ($row = mysqli_fetch_assoc($result)) {
+                                                        echo '<tr>
+            <td>', $row['code'], '</td>
+            <td>', $row['name'], '</td>
+            <td>', $row['NVQ_level'], '</td>
+            <td>', $row['department_code'], '</td>
+            
+            
+            <td>
+            <div class="row">
+            <div class="col"></div>
+            <div class="col-auto">
+            <div class="btn-group btn-sm" role="group" aria-label="Basic example">
+            <a href="course.php?edit=', $row['id'], '" class="btn btn-warning" > <img src="https://img.icons8.com/android/18/000000/edit.png"/> </a> 
+            <a href="?delete=', $row['id'], '" class="btn btn-danger"><img src="https://img.icons8.com/windows/18/000000/delete-forever.png"/> </a>
+            <a href="modules.php?view=', $row['code'], '" class="btn btn-success"><b>modules</b> </a>
+            </div>
+            </div>
+          </div>
+            </td>
+        </tr>';
+                                                    }
+                                                } else {
+                                                    echo 'no rows';
+                                                }
+                                                ?>
+
+                                            </tbody>
+                                        </table>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+
+                    </div>
+                    <br>
+                    <!-- 2 row end -->
+                </div>
+
+
+
+                <!-- 1st row end -->
+                <!-- <div id="overlay" class="overlay"></div>
+            <div class="container-fluid p-5"> -->
+                <!-- #1 Insert Your Content-->
+                <!-- <div class="container"> -->
+                <!-- 2 row start -->
+
+
+
+        </div>
+        </form>
+
+
+
+    </div>
+    <!-- #1 Insert Your Content" -->
+    </div>
+    </main>
+    </div>
+    <?php include_once("../script.php"); ?>
+</body>
+
+</html>
---
+++ lecturers/departments.php
@@ -0,0 +1,144 @@
+<?php
+$title = "Add Department | Online Examination Result Management System | SLGTI";
+$description = "Online Examination Result  Management System (ERMS)-SLGTI";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include_once("./head.php"); ?>
+    <?php include_once("../config.php"); ?>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+
+<body>
+    <div class="page-wrapper toggled bg2 border-radius-on light-theme">
+
+        <?php include_once("nav.php"); ?>
+
+        <!-- <main class="page-content pt-2"> -->
+        <!-- 1st row start -->
+
+        <div class="container">
+            <!-- delete -->
+<?php
+if(isset($_GET['delete'])){
+    $id = $_GET['delete'];
+    $sql = "select department_code from courses where department_code='$id';";
+    $result = mysqli_query($con,$sql);
+    if(mysqli_num_rows($result)>0)
+    {
+        ?>
+       <div class='alert alert-warning' role='alert'>
+       This department already allocate courses so not delete this department.
+       <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+           <span aria-hidden='true'>&times;</span>
+        </button>
+      </div>
+        <?php
+    }  
+    else{
+    $sql = "DELETE FROM `departments` WHERE `departments`.`code` = '$id'";
+    if(mysqli_query($con,$sql)){
+        echo 'Record was deleted';
+    }else{
+        echo 'Try again';
+    }
+}
+}
+?>
+<!-- delete -->
+            <br>
+            <form action="">
+                <div class="card  mb-3">
+                    <div class="card-header ">
+                        <div class="row">
+                            <div class="col">
+                                <h4>Department</h4>
+                            </div>
+                            <div class="col-auto">
+                                <a href="department.php" class="btn btn-outline-primary">New</a>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="card-body ">
+
+                        <!-- select -->
+
+                        <form method="POST">
+                            <div class="row">
+                                <div class="form-group col-md-12">
+                                    <div class="table-responsive-sm">
+                                        <table class="table ">
+                                            <thead class="p-3 mb-2 bg-primary text-white">
+                                                <tr>
+                                                    <th scope="col">code</th>
+                                                    <th scope="col">Name</th>
+                                                    <div class="row"></div>
+                                                    <th class="text-right"></th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php
+                                                $sql = 'SELECT * FROM `departments`';
+                                                $result = mysqli_query($con, $sql);
+                                                if (mysqli_num_rows($result) > 0) {
+                                                    while ($row = mysqli_fetch_assoc($result)) {
+                                                        echo '<tr>
+            <td>', $row['code'], '</td>
+            <td>', $row['name'], '</td>
+            
+            <td>
+            <div class="row">
+            <div class="col"></div>
+            <div class="col-auto">
+            <div class="btn-group btn-sm" role="group" aria-label="Basic example">
+            <a href="department.php?edit=', $row['id'], '" class="btn btn-warning" > <img src="https://img.icons8.com/android/18/000000/edit.png"/> </a> 
+            <a href="?delete=', $row['code'], '" class="btn btn-danger"><img src="https://img.icons8.com/windows/18/000000/delete-forever.png"/> </a>
+            <a href="courses.php?view=', $row['code'], '" class="btn btn-success"><b>courses</b> </a>
+            </div>
+            </div>
+          </div>
+            </td>
+        </tr>';
+                                                    }
+                                                } else {
+                                                    echo 'no rows';
+                                                }
+                                                ?>
+
+
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+
+                    </div>
+                    <br>
+                    <!-- 2 row end -->
+
+
+
+                </div>
+
+
+
+
+
+        </div>
+        </form>
+
+
+
+    </div>
+    <!-- #1 Insert Your Content" -->
+    </div>
+    </main>
+    </div>
+    <?php include_once("../script.php"); ?>
+</body>
+
+</html>
