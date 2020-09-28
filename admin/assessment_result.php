d3989a0ee2ff8bd96bc4e15d71580e75eab83846
sumanan24 <48612693+sumanan24@users.noreply.github.com>
2020-09-28 15:58:57 +0530

    Update assessment_result.php

--- admin/assessment_result.php
+++ admin/assessment_result.php
@@ -92,46 +92,7 @@ if(isset($_GET['id'])){
                         <h6 class="text-muted"><?php echo $course;?></h6>
                     </div>
 
-                    <div class="col-md-2 col-sm-12">
-                        <h6>Attempt </h6>
-                    </div>
-                    <div class="col-md-4 col-sm-12">
-                        <h6 class="text-muted">01</h6>
-                    </div>
-
-                    <div class="col-md-2 col-sm-12">
-                        <h6>Date </h6>
-                    </div>
-                    <div class="col-md-4 col-sm-12">
-                        <h6 class="text-muted"><input type='date'disabled value="2020-02-02"></h6>
-                    </div>
-
-                    <div class="col-md-2 col-sm-12">
-                        <h6>Attempt </h6>
-                    </div>
-                    <div class="col-md-4 col-sm-12">
-                        <h6 class="text-muted">02</h6>
-                    </div>
-
-                    <div class="col-md-2 col-sm-12">
-                        <h6>Date </h6>
-                    </div>
-                    <div class="col-md-4 col-sm-12">
-                        <h6 class="text-muted"><input type='date'disabled value="2020-02-15"></h6>
-                    </div>
-                    <div class="col-md-2 col-sm-12">
-                        <h6>Attempt </h6>
-                    </div>
-                    <div class="col-md-4 col-sm-12">
-                        <h6 class="text-muted">03</h6>
-                    </div>
-
-                    <div class="col-md-2 col-sm-12">
-                        <h6>Date </h6>
-                    </div>
-                    <div class="col-md-4 col-sm-12">
-                        <h6 class="text-muted"><input type='date'></h6>
-                    </div>
+                    
                 </div>
                 
                 
