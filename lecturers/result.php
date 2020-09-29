133570b0d23a68e9d5e1f2d6043311a19378bc76
Kiriyabala <kiriyabalasubramaniyam@gmail.com>
2020-09-29 09:19:50 +0530

    #add

--- lecturers/result.php
+++ lecturers/result.php
@@ -1,4 +1,16 @@
 <?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header('Location: .././index.php');
+}
+?>
+<?php
+if (isset($_GET['logout']) && isset($_SESSION['username'])) {
+    unset($_SESSION['username']);
+    header('Location: .././index.php');
+}
+?>
+<?php
 $title = " Add Assessment| Online Examination Result Management System | SLGTI";
 $description = "Online Examination Result  Management System (ERMS)-SLGTI";
 ?>
