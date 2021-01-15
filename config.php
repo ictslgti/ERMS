cab69a1caeaf77315793f6b215408bfa2ef42d0b
sumanan24 <48612693+sumanan24@users.noreply.github.com>
2021-01-15 13:21:19 +0530

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASS', '' );
 define( 'DB_NAME', 'erms' );
 
-$con = mysqli_connect( 'localhost', 'root', '', 'erms' );
+$con = mysqli_connect( 'localhost', ' ermsuser', '!23$cfDSrt67', 'erms' );
 if ( mysqli_connect_errno() ) {
     echo 'Failed to connect to MySQL: ' . mysqli_connect_error();
 
