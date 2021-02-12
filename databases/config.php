fcc03b27c528b5310989c3f722a2dfd109d2387c
sumanan24 <48612693+sumanan24@users.noreply.github.com>
2021-02-12 11:37:08 +0530

    Update config.php

--- databases/config.php
+++ databases/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = new mysqli('tvec.achchuthan.org','ermsuser','!23$cfDSrt67','erms');
+$con = new mysqli('mis.achchuthan.org','ermsuser','!23$cfDSrt67','erms');
 if ( mysqli_connect_errno() ) {
     echo 'Failed to connect to MySQL: ' . mysqli_connect_error();
 
